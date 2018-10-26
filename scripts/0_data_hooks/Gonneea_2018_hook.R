7db54386ecfcfd7740ac420bbc49efb4e560f73b
dklinges9 <klingesd@si.edu>
2018-10-26 09:22:20 -0400

    updates to Gonneea hook

--- scripts/0_data_hooks/Gonneea_2018_hook.R
+++ scripts/0_data_hooks/Gonneea_2018_hook.R
@@ -4,8 +4,11 @@
 # This script hooks in data from the Holmquist et al 2018 Scientific Reports
 #   data release. 
 
+
+## Download data ########################
 # Load RCurl, a package used to download files from a URL
 library(RCurl)
+library(tidyverse)
 
 # The Gonneea et al (2018) data release features a diverse suite of file types:
 #   a .jpg, a .xlsx, a .csv, and a .xml
@@ -17,22 +20,54 @@ url_list <- list("https://www.sciencebase.gov/catalog/file/get/5a748e35e4b00f54e
                  "https://www.sciencebase.gov/catalog/file/get/5a748e35e4b00f54eb19f96c?f=__disk__3e%2F2d%2Ff5%2F3e2df544c537a35007214d1fe595b45499df2f4a")
 
 # Extract Saltmarsh_AR.jpg
-download.file(url_list[[1]], paste0(getwd(), "./data/Gonneea_2018/Saltmarsh_AR.jpg"),
+download.file(url_list[[1]], paste0(getwd(), "./data/Gonneea_2018/original/Saltmarsh_AR.jpg"),
               mode = "wb")
 
 # Extract Waquoit_Core_data_release.xlsx
-download.file(url_list[[2]], paste0(getwd(), "./data/Gonneea_2018/Waquoit_Core_data_release.xlsx"), 
+download.file(url_list[[2]], paste0(getwd(), "./data/Gonneea_2018/original/Waquoit_Core_data_release.xlsx"), 
               mode = "wb")
 
 # Extract Waquoit_Core_data_release.csv
-download.file(url_list[[3]], paste0(getwd(), "./data/Gonneea_2018/Waquoit_Core_data_release.csv"),
+download.file(url_list[[3]], paste0(getwd(), "./data/Gonneea_2018/original/Waquoit_Core_data_release.csv"),
               mode = "wb")
 
 # Extract Waquoit_Core_data_release_meta.xml
-download.file(url_list[[4]], paste0(getwd(), "./data/Gonneea_2018/Waquoit_Core_data_release_meta.xml"),
+download.file(url_list[[4]], paste0(getwd(), "./data/Gonneea_2018/original/Waquoit_Core_data_release_meta.xml"),
               mode = "wb")
 
 
+## Curate data to CCRCN Structure ########################
+
+# Import data file into R
+Gonneea_2018 <- read_csv("./data/Gonneea_2018/original/Waquoit_Core_data_release.csv", col_names = TRUE)
+
+# Change column names to values of first row
+# Why? Because the top 2 rows were both dedicated to column headers
+new_colnames <- c(Gonneea_2018 %>%
+  slice(1))
+colnames(Gonneea_2018) <- new_colnames
+Gonneea_2018 <- Gonneea_2018 %>%
+  slice(2:561)
+
+# Change all no data values to "NA"
+Gonneea_2018 <- Gonneea_2018 %>%
+  na_if(-99999) # Changes all "-99999" values to "NA"
+
+# Rename attributes that are already properly formatted
+Gonneea_2018 <- Gonneea_2018 %>%
+  rename(core_id = "ID") %>%
+  rename(core_date = "Date") %>%
+  rename(core_latitude = "Lat") %>%
+  rename(core_longitude = "Lon") %>%
+  rename(core_elevation = "Elevation") %>%
+  rename(dry_bulk_density = "DBD") %>%
+  rename(dry_bulk_density = "DBD") %>%  
+  
+  
+  # warp depth
+  # ours: Pb: 1 becquerel = 1 disintegration/second/kilogram
+  #   her data is in 1 disintegration/g/gram
+  mutate(depth_min = Depth)
 
 
 
