119dcc55f0aae645c5bb442ca6fc86c3bbaea611
dklinges9 <klingesd@si.edu>
2018-12-04 17:08:17 -0500

    curated Sanderman US site, core, species data

--- scripts/0_data_hooks/Sanderman_2018_hook.R
+++ scripts/0_data_hooks/Sanderman_2018_hook.R
@@ -7,11 +7,10 @@
 
 library(tidyverse)
 library(RCurl)
+library(readxl)
 
 ## Add Sanderman mangrove sites ############
 
-
-soil_profiles <- read.csv(text=getURL("https://raw.githubusercontent.com/Smithsonian/CCRCN-Data-Library/master/data/CCRCN_synthesis/CCRCN_core_data.csv"))
 
 # Rename and recode attributes to be congruent with core data
 Sanderman_2018 <- Sanderman_2018 %>%
@@ -34,4 +33,74 @@ Sanderman_2018 <- Sanderman_2018 %>%
 
 # Save as output data
 write.csv(Sanderman_2018, "./data/Sanderman_2018/derivative/Sanderman_2018_core_data.csv")
+
+
+## Curate Sanderman USA mangroves ############
+
+sanderman_2018_USA <- read_excel("./data/Sanderman_2018/original/mangroves for USA.xlsx",
+                              sheet = 2)
+
+
+# Recode
+sanderman_2018_USA <- sanderman_2018_USA %>%
+  rename(study_id = "Source") %>%
+  mutate(study_id = gsub(" ", "_", study_id)) %>%
+  rename(site_id = "Site name") %>%
+  mutate(site_id = gsub(" ", "_", site_id)) %>%
+  rename(core_id = "Site #") %>%
+  rename(site_description = "Location") %>%
+  rename(country = "Country") %>%
+  rename(core_latitude = "Latitude") %>%
+  rename(core_longitude = "Longitude") %>%
+  rename(core_position_accuracy_flag = "accuracy_flag") %>%
+  rename(core_position_accuracy = "approx_acc") %>%
+  mutate(vegetation_class = "forested") %>%
+  mutate(vegetation_notes = "mangrove") %>%
+  rename(landscape_position = "Landscape position") %>%
+  rename(core_date = "Years_collected") %>%
+  rename(core_length = "total_depth") %>%
+  rename(core_length_flag = "full_profile") %>%
+  select(-`Landscape Unsure`, -`HGM Unsure`, -`Dominant species`, -`Mangrove type`)
+
+sanderman_2018_USA$core_length_flag <- recode(sanderman_2018_USA$core_length_flag, 
+                                     Y = "core depth represents deposit depth",
+                                     N = "core depth limited by length of corer")
+
+## Site level data ##############
+site_data <- sanderman_2018_USA %>%
+  select(study_id, site_id, site_description, country, vegetation_class, 
+         vegetation_notes, landscape_position)
+  
+## Core level data ##############
+
+core_data <- sanderman_2018_USA %>%
+  select(study_id, site_id, core_id, country, core_latitude, core_longitude, 
+         core_position_accuracy_flag, core_position_accuracy, core_date, core_length, 
+         core_length_flag)
+
+## Species data #############
+
+species_data <- site_data %>%
+  select(study_id, site_id, core_id, `Dominant species`) %>%
+  rename(species_code = "Dominant species")
+
+# Species were input with multiple species per row. Split up so that each species
+#   entry gets a row
+species_data<- species_data %>% 
+  mutate(species_code = strsplit(as.character(species_code), ",")) %>% 
+  unnest(species_code)
+
+## Study level metadata ###############
+study_metadata <- site_data %>%
+  rename(email = "Data_owner")
+
+
+## Write data ###############
+
+write.csv(site_data, "./data/Sanderman_2018/derivative/Sanderman_2018_site_data_US.csv")
+  
+write.csv(core_data, "./data/Sanderman_2018/derivative/Sanderman_2018_core_data_US.csv")
+
+write.csv(species_data, "./data/Sanderman_2018/derivative/Sanderman_2018_species_data_US.csv")
+
 
