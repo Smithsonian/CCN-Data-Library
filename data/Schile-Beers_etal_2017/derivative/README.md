d4fa1277f81e54dd581a66611c4c3bfd56b46c31
Michael Lonneman <mlonneman@gmail.com>
2019-01-24 12:24:50 -0500

    Completed Schile-Beers et al. 2017 hook script and data ingestion

---
+++ data/Schile-Beers_etal_2017/derivative/README.md
@@ -0,0 +1,27 @@
+# Data citation: 
+# Schile, Lisa M. and Megonigal, J. Patrick. 2017. [Dataset] 
+# "Abu Dhabi Blue Carbon Demonstration Project." Distributed by Smithsonian Environmental Research Center. https://doi.org/10.5479/data_serc/10088/31949
+
+# Journal article citation: 
+# Schile, L. M., Kauffman, J. B., Crooks, S., Fourqurean, J. W., Glavan, J. and Megonigal, J. P. (2017), Limits on carbon sequestration in
+# arid blue carbon ecosystems. Ecol Appl, 27: 859–874. doi:10.1002/eap.1489
+
+## Site level, core level and soil depth series data
+
+Three .csv files are provided:
+
+1. “Schile-Beers_etal_2017_core_data.csv"
+
+2. "Schile-Beers_etal_2017_depth_series_data.csv"
+
+3. "Schile-Beers_etal_2017_site_data.csv"
+
+Datasets follow the structure and definitions given at CCRCN-Soils-Working-Group/data/data-syntheses/data_templates/ in the group’s GitHub, and at https://serc.si.edu/coastalcarbon/database-structure
+
+## Issues: 
+
+1. Two sites have multi-year entries for cores that either do not have matching depth series data or have depth series data but no clear core-level metadata (and therefore no location data). The specific site_ids are: Eastern Mangrove 10 yr, Eastern Mangrove 7 yr, Eastern Mangrove 3 yr, Jubail Is. 10 yr, Jubail Is. 7 yr, Jubail Is. 3 yr
+
+2. According to the methods in the publication, core depth was to either 3 m (the corer was 1 m long) or until parent material. There is no clear core_depth_flag code for the former, and I have coded it as "core depth limited by length of corer"
+
+3. There are biomass data available in the original data but at this moment there are no guidelines for incorporating them into the CCRCN. 
--- scripts/0_data_hooks/Schile-Beers_2017_hook.R
+++ scripts/0_data_hooks/Schile-Beers_2017_hook.R
@@ -74,9 +74,6 @@ Schile_2017_depth_series_data <- read_excel(paste0(FILE_PATH, FILE_NAME), sheet=
 
 ## Depth series data ###################
 
-# Call functions from curation_functions script
-source("./scripts/1_data_formatting/curation_functions.R") 
-
 # Issues: 
 # 1. Two sites have multi-year entries for cores that either do not have matching depth series data 
 # or have depth series data but no clear core-level metadata (and therefore no location data) 
@@ -139,112 +136,27 @@ write.csv(Schile_2017_core_data, "./data/Schile-Beers_etal_2017/derivative/Schil
 #   data up to the site level
 
 # Change value of digits so we don't have too many for the next step
-# options(digits=6)
-# 
-# # Change all attributes to numeric. we'll need this later for aggregate the data
-# #   to the site level
-# Osland_2018_site_data <- sapply(Osland_2018_land_climate, as.numeric)
-# Osland_2018_site_data <- as.data.frame(Osland_2018_site_data) # Turn back to df
-# 
-# # This turns site and core ID attributes to NA, so we'll need to add those back.
-# #   We'll pull them from the core level data
-# # NOTE: this is a dangerous cbind. If the data was re-ordered, the data will be
-# #   joined incorrectly
-# Osland_2018_site_data <- cbind(Osland_2018_site_data, site_id = Osland_2016_core_data$site_id,
-#                                core_id = Osland_2016_core_data$core_id)
-# 
-# # Rename and curate
-# Osland_2018_site_data <- Osland_2018_site_data %>%
-#   select(-ID, -tran, -plot, -estuary, -core_id, -dist) %>%
-#   rename(core_longitude = "lon") %>%
-#   rename(core_latitude = "lat") %>%
-#   rename(mean_annual_precip = "MAP") %>%
-#   rename(aridity_index = "AI") %>%
-#   rename(potential_evapotrans = "PET") %>%
-#   rename(min_temp = "Tmin") %>%
-#   mutate(study_id = "Osland_2018")
-# 
-# # Find min and max lat/long for each site
-# source("./scripts/1_data_formatting/curation_functions.R") 
-# Osland_2018_site_data_boundaries <- create_multiple_geographic_coverages(Osland_2018_site_data)
-# Osland_2018_site_data <- Osland_2018_site_data %>%
-#   left_join(Osland_2018_site_data_boundaries) %>% # Add site bounds in
-#   select(-core_latitude, -core_longitude)
-# # remove NAs before aggregation
-# Osland_2018_site_data <- na.omit(Osland_2018_site_data) 
-# 
-# # Now aggeregate data to the site level
-# Osland_2018_site_data <- Osland_2018_site_data %>%
-#   group_by(site_id) %>%
-#   summarize_all(mean)
-# 
-# # Write data
-# write.csv(Osland_2018_site_data, "./data/Osland_2018/derivative/Osland_2018_site_data.csv")
-# 
-# ## Species data ##################
-# 
-# # Select only the columns that correspond to the species presence in a 1-m2
-# #   plot surrounding each core, plus the necessary other data
-# Osland_2018_species_data <- Osland_2018_veg %>%
-#   select(1:182) %>%
-#   select("estuary", "plot", 16:182)
-# 
-# # Rename site and core IDs
-# Osland_2018_species_data <- Osland_2018_species_data %>%
-#   rename(site_id = "estuary") %>%
-#   # current core IDs are not unique, so concatenate with site IDs
-#   mutate(core_id = paste0(site_id, "_", plot)) %>%
-#   select(-plot) %>%
-#   mutate(study_id = "Osland_2018")
-# 
-# # The species presence data is currently  wide-form, with each column
-# #   corresponding to the fraction area occupied by each plant species.
-# # Let's write a function that can be applied to each row to change the data to
-# #   long-form.
-# find_max <- function(df, first_col, last_col) {
-#   df <- df %>%
-#     # Gather the species columns into one column and select just the most dominant
-#     #   species present per core.
-#     gather(colname, value, first_col:last_col) %>%
-#     mutate(value = value) %>%
-#     # Choose just the most dominant plant species
-#     filter(value == max(value))
-#   
-#   # If all of the values for the species presence columns are 0, then change the
-#   #   values to NA and only use the first row. The species presence value for
-#   #   the given core will now just be NA.
-#   if (df$value[[1]] == 0) {
-#     df$value <- NA
-#     df <- slice(df, 1)
-#   }
-#   df
-# }
-# 
-# for (i in 1:nrow(Osland_2018_species_data)) {
-#   
-#   df <- slice(Osland_2018_species_data, i)
-#   if (i == 1) {
-#     out <- find_max(df, 5, 168)
-#   } else {
-#     
-#     out <- rbind(out, find_max(df, 5, 168))
-#   }
-# }
-# 
-# colname <- out$colname
-# 
-# Osland_2018_species_data <- out %>%
-#   rename(species_code = "colname") %>%
-#   rename(fraction_coverage = "value") %>%
-#   mutate(fraction_coverage = as.numeric(fraction_coverage)) %>%
-#   select(site_id, core_id, species_code, fraction_coverage)
-# 
-# # Remove the '1c' string from the species_code entries
-# Osland_2018_species_data$species_code <- 
-#   sapply(Osland_2018_species_data$species_code,
-#          function(x) {
-#            gsub("1c", "", x) # Replace '1c' with ''                                           
-#          })
-# 
-# # Write out data
-# write.csv(Osland_2018_species_data, "./data/Osland_2018/derivative/Osland_2018_species_data.csv")
+options(digits=6)
+
+# Rename and curate
+Schile_2017_site_data <- Schile_2017_core_data %>%
+  select(site_id, core_id, study_id, core_latitude, core_longitude, core_elevation, vegetation_notes) 
+  
+# Find min and max lat/long for each site
+source("./scripts/1_data_formatting/curation_functions.R")
+Schile_2017_site_data_boundaries <- create_multiple_geographic_coverages(Schile_2017_site_data)
+Schile_2017_site_data <- Schile_2017_site_data %>%
+  left_join(Schile_2017_site_data_boundaries) %>% # Add site bounds in
+  select(-core_latitude, -core_longitude)
+# remove NAs before aggregation
+Schile_2017_site_data <- na.omit(Schile_2017_site_data)
+
+# Now aggeregate data to the site level
+Schile_2017_site_data <- Schile_2017_site_data %>%
+  group_by(site_id) %>%
+  summarize(study_id = first(study_id), mean_elevation = mean(core_elevation), 
+            site_longitude_max = first(site_longitude_max), site_longitude_min = first(site_longitude_min),
+            site_latitude_max = first(site_latitude_max), site_latitude_min = first(site_latitude_min))
+
+# Write data
+write.csv(Schile_2017_site_data, "./data/Schile-Beers_etal_2017/derivative/Schile_2017_site_data.csv")
