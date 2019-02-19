d033b2efd92b6f20f3b9fbc5d69b25a9020069a3
David <klingesd@si.edu>
2019-02-19 18:05:00 -0500

    added site IDs back to Holmquist 2018 data

--- scripts/0_data_hooks/Holmquist_2018_hook.R
+++ scripts/0_data_hooks/Holmquist_2018_hook.R
@@ -18,23 +18,28 @@
 library(RCurl)
 library(tidyverse)
 
-# Create a list of the URLs for each data file
-url_list <- list("https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_core_data.csv?sequence=7&isAllowed=y",
-                 "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_depth_series_data.csv?sequence=8&isAllowed=y",
-                 "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_impact_data.csv?sequence=9&isAllowed=y",
-                 "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_methods_data.csv?sequence=10&isAllowed=y",
-                 "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_species_data.csv?sequence=11&isAllowed=y"
-)
+## NOTE: this section commented out, but kept, because the data for the Holmquist
+# 2018 Sci Reports data release does not include site IDs. If and when this data
+# release is fixed we'll re-incorporate these lines, until then using internal
+# data files
 
-# Apply a function, which downloads each of the data files, over url_list
-lapply(url_list, function(x) {
-  # Extract the file name from each URL
-  filename <- as.character(x)
-  filename <- substring(filename, 56)
-  filename <- gsub("\\..*","", filename)
-  # Now download the file into the "data" folder
-  download.file(x, paste0(getwd(), "./data/Holmquist_2018/", filename, ".csv"))
-})
+# # Create a list of the URLs for each data file
+# url_list <- list("https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_core_data.csv?sequence=7&isAllowed=y",
+#                  "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_depth_series_data.csv?sequence=8&isAllowed=y",
+#                  "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_impact_data.csv?sequence=9&isAllowed=y",
+#                  "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_methods_data.csv?sequence=10&isAllowed=y",
+#                  "https://repository.si.edu/bitstream/handle/10088/35684/V1_Holmquist_2018_species_data.csv?sequence=11&isAllowed=y"
+# )
+# 
+# # Apply a function, which downloads each of the data files, over url_list
+# lapply(url_list, function(x) {
+#   # Extract the file name from each URL
+#   filename <- as.character(x)
+#   filename <- substring(filename, 56)
+#   filename <- gsub("\\..*","", filename)
+#   # Now download the file into the "data" folder
+#   download.file(x, paste0(getwd(), "./data/Holmquist_2018/", filename, ".csv"))
+# })
 
 ## 2. Import data to convert codes to common plain language ####
 
