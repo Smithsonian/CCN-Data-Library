8ca06719e9d27f6c818bae5915139fbdfa21081b
David <klingesd@si.edu>
2019-02-19 10:22:34 -0500

    quick edits to lit compiler interface

---
+++ CCRCN_lit_compiler/global.R
@@ -0,0 +1,8 @@
+# Coastal Carbon Research Coordination Network
+# This is the global script for two-file version of the CCRCN lit compiler
+# interactive interface.
+# Contact: klingesd@si.edu
+
+## Workspace Prep ################
+
+library(rcrossref)
