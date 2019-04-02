e332c1c5681fdcf34f64b338f9ca4587417159ad
David <klingesd@si.edu>
2019-04-01 23:19:13 -0400

    curated Jones 2017 geochron and LOI data
    see notes in readme about two differente radiocarbon age measurements

--- scripts/0_data_hooks/Jones_2017_hook.R
+++ scripts/0_data_hooks/Jones_2017_hook.R
@@ -3,21 +3,153 @@
 #   the Soil Carbon working group.
 # Contact: klingesd@si.edu
 
-## Workspace prep ########################
+## 1. Workspace prep ########################
 
 library(tidyverse)
 
-depthseries_raw <- read_csv("./data/Jones_2017/original/Jones_2017_depthseries.csv",
-                        col_names = FALSE)
+# Site data
+sites <- read_csv("data/Jones_2017/derivative/intermediate/Jones_2017_HandEnteredSiteCoreDataFromNeotoma.csv")
+
+# Downloaded geochrono datasets
+geochron_oligo <- read_csv("data/Jones_2017/derivative/intermediate/dataset25338.csv")
+geochron_heavy_salt <- read_csv("data/Jones_2017/derivative/intermediate/dataset25345.csv")
+geochron_mod_salt <- read_csv("data/Jones_2017/derivative/intermediate/dataset25364.csv")
+
+# Downloaded LOI datasets
+LOI_oligo_raw <- read_csv("data/Jones_2017/original/dataset25339.csv")
+LOI_heavy_salt_raw  <- read_csv("data/Jones_2017/original/dataset25346.csv")
+LOI_mod_salt_raw  <- read_csv("data/Jones_2017/original/dataset25365.csv")
+
+# Depthseries data digitized from figure
+depthseries_figure <- read_csv("./data/Jones_2017/original/Jones_2017_depthseries.csv",
+                            col_names = FALSE)
 
 
-## Curate depthseries data #########
 
-depthseries <- depthseries_raw %>%
+## 2. Curate Site data ############
+
+# Imported file is already curated to CCRCN standards
+
+## 3. Core data ############
+
+core_data <- sites %>%
+  mutate(study_id = "Jones_et_al_2017") %>%
+  select(study_id, site_id, core_id)
+
+## 4. Depthseries data ############
+
+## ....4a. Geochronlogy data ############
+geochron_oligo <- geochron_oligo %>%
+  mutate(core_id = "Oligohaline_Marsh")
+
+geochron_heavy_salt <- geochron_heavy_salt %>%
+  mutate(core_id = "Heavily_Salt_Impacted_Swamp")
+
+geochron_mod_salt <- geochron_mod_salt %>%
+  mutate(core_id = "Moderately_Salt_Impacted")
+
+source("./scripts/1_data_formatting/curation_functions.R")  
+geochron <- geochron_oligo %>%
+  bind_rows(geochron_heavy_salt) %>%
+  bind_rows(geochron_mod_salt) %>%
+  mutate(study_id = "Jones_et_al_2017") %>%
+  rename(c14_age_geochron = Age) %>%
+  mutate(age_depth_model_reference = "YBP") %>%
+  mutate(depth_min = Depth - Thickness) %>%
+  mutate(depth_max = Depth + Thickness) %>%
+  rename(material_dated = MaterialDated, age_depth_model_notes = Notes,
+         c14_age_sd_geochron = ErrorOlder) %>%
+  mutate(age_depth_model_notes = ifelse(!is.na(age_depth_model_notes), 
+      "c14 age yielded greater than modern day", age_depth_model_notes)) %>%
+  select(study_id, core_id, depth_min, depth_max, c14_age_geochron, c14_age_sd_geochron, 
+         material_dated, age_depth_model_reference, age_depth_model_notes)
+
+## ....4b. LOI data ############
+
+# Coerce into matrix and transpose
+LOI_oligo <- t(as.matrix(LOI_oligo_raw))
+LOI_oligo <- as.data.frame(LOI_oligo)
+LOI_oligo <- as_tibble(LOI_oligo, rownames = NULL)
+
+# Curate
+LOI_oligo <- LOI_oligo %>%
+  slice(-1:-5) %>%
+  mutate(core_id = "Oligohaline_Marsh") %>%
+  separate(`V1`, into = c("depth_min", "depth_max"), sep = "-") %>%
+  mutate(depth_max = as.double(gsub(" cm", "", depth_max))) %>%
+  separate("V6", into = c("error_max", "c14_age_LOI", "error_min"), sep = "/") %>%
+  mutate(error_max = as.double(error_max), c14_age_LOI = as.double(c14_age_LOI),
+         error_min = as.double(error_min)) %>%
+  mutate(c14_age_sd_LOI = ((error_max - c14_age_LOI) + (c14_age_LOI - error_min)) / 2) %>%
+  rename(dry_bulk_density = "V7", loss_on_ignition = "V8") %>%
+  select(core_id, depth_min, depth_max, dry_bulk_density, loss_on_ignition, 
+         c14_age_LOI, c14_age_sd_LOI)
+
+# Coerce into matrix and transpose
+LOI_heavy_salt <- t(as.matrix(LOI_heavy_salt_raw))
+LOI_heavy_salt <- as.data.frame(LOI_heavy_salt)
+LOI_heavy_salt <- as_tibble(LOI_heavy_salt, rownames = NULL)
+
+# Curate
+LOI_heavy_salt <- LOI_heavy_salt %>%
+  slice(-1:-5) %>%
+  mutate(core_id = "Heavily_Salt_Impacted_Swamp") %>%
+  separate(`V1`, into = c("depth_min", "depth_max"), sep = "-") %>%
+  mutate(depth_max = as.double(gsub(" cm", "", depth_max))) %>%
+  separate("V6", into = c("error_max", "c14_age_LOI", "error_min"), sep = "/") %>%
+  mutate(error_max = as.double(error_max), c14_age_LOI = as.double(c14_age_LOI),
+         error_min = as.double(error_min)) %>%
+  mutate(c14_age_sd_LOI = ((error_max - c14_age_LOI) + (c14_age_LOI - error_min)) / 2) %>%
+  rename(dry_bulk_density = "V7", loss_on_ignition = "V8") %>%
+  select(core_id, depth_min, depth_max, dry_bulk_density, loss_on_ignition, 
+         c14_age_LOI, c14_age_sd_LOI)
+
+# Coerce into matrix and transpose
+LOI_mod_salt <- t(as.matrix(LOI_mod_salt_raw))
+LOI_mod_salt <- as.data.frame(LOI_mod_salt)
+LOI_mod_salt <- as_tibble(LOI_mod_salt, rownames = NULL)
+
+# Curate
+LOI_mod_salt <- LOI_mod_salt %>%
+  slice(-1:-5) %>%
+  mutate(core_id = "Moderately_Salt_Impacted") %>%
+  separate(`V1`, into = c("depth_min", "depth_max"), sep = "-") %>%
+  mutate(depth_max = as.double(gsub(" cm", "", depth_max))) %>%
+  separate("V6", into = c("error_max", "c14_age_LOI", "error_min"), sep = "/") %>%
+  mutate(error_max = as.double(error_max), c14_age_LOI = as.double(c14_age_LOI),
+         error_min = as.double(error_min)) %>%
+  mutate(c14_age_sd_LOI = ((error_max - c14_age_LOI) + (c14_age_LOI - error_min)) / 2) %>%
+  rename(dry_bulk_density = "V7", loss_on_ignition = "V8") %>%
+  select(core_id, depth_min, depth_max, dry_bulk_density, loss_on_ignition, 
+         c14_age_LOI, c14_age_sd_LOI)
+
+
+# Join LOI datasets
+LOI <- LOI_oligo %>%
+  bind_rows(LOI_heavy_salt) %>%
+  bind_rows(LOI_mod_salt) %>%
+  mutate(depth_min = as.double(depth_min)) %>%
+  mutate(depth_max = as.double(depth_max)) %>%
+  mutate(study_id = "Jones_et_al_2017")
+
+## ....4c. Combine depthseries datasets ##############
+
+depthseries <- geochron %>%
+  full_join(LOI, by = c("core_id", "depth_min", "depth_max", "study_id"))
+
+# Data digitized from depthseries figure...still not sure what core(s) this 
+#   corresponds to
+depthseries_figure <- depthseries_figure %>%
   rename(depth = X1, age = X2)
 
 
 
 ## Write data ###########
 
-write_csv(depthseries, "./data/Jones_2017/derivative/Jones_2017_depthseries.csv")
+write_csv(sites, "./data/Jones_2017/derivative/final/Jones_2017_sites.csv")
+
+write_csv(core_data, "./data/Jones_2017/derivative/final/Jones_2017_cores.csv")
+
+write_csv(depthseries, "./data/Jones_2017/derivative/final/Jones_2017_depthseries.csv")
+
+write_csv(depthseries_figure, "./data/Jones_2017/derivative/final/Jones_2017_depthseries_from_figure.csv")
