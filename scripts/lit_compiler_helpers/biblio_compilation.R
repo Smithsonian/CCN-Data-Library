9a9aac5c27a367b7f81b0a8fecc3d84c94eba003
David <klingesd@si.edu>
2019-04-22 14:32:01 -0400

    joinined study IDs to RCN biblio, work in progress

--- scripts/lit_compiler_helpers/biblio_compilation.R
+++ scripts/lit_compiler_helpers/biblio_compilation.R
@@ -7,28 +7,37 @@
 
 ## Workspace prep ########
 library(RefManageR)
+library(fuzzyjoin)
+library(tidyverse)
+
+## Curate Bibliography ###############
+
+# import the CCRCN bibliography 
+CCRCN_bib <- bib2df("./docs/bibliography/CCRCN_bibliography.bib")
+
+study_ids <- CCRCN_coredata %>%
+  distinct(study_id) %>%
+  select(study_id) %>%
+  arrange(study_id)
+
+# fuzzyjoin
+CCRCN_bib_df <- fuzzy_join(study_ids, CCRCN_bib, by = c("study_id" = "BIBTEXKEY"), 
+                        match_fun = str_detect, mode = "full")
+
+# Create a df of just study_id and BIBTEXKEY to link bib entries
+CCRCN_bib_link <- CCRCN_bib_df %>%
+  select(study_id, BIBTEXKEY)
 
 
-# Drake et al 2015
-cr_cn("10.1007/s00267-015-0568-z")
+## Write bibliography ################
 
-# Radabaugh et al 2017
-cr_cn("10.1007/s12237-017-0362-7")
+# Write out as .csv
+write_csv(CCRCN_bib_link, "./docs/bibliography/CCRCN_bibliography_link.csv")
 
-cr_cn("")
-
-@article{crooks2014coastal,
-  title={Coastal blue carbon opportunity assessment for the Snohomish Estuary: The climate benefits of estuary restoration},
-  author={Crooks, S and Rybczyk, J and O’Connell, Keeley and Devier, DL and Poppe, K and Emmett-Mattox, S},
-  journal={Report by Environmental Science Associates, Western Washington University, EarthCorps, and Restore America’s Estuaries},
-  year={2014}
-}
-
-bib <- ReadCrossRef(query = '10.1080/01621459.2012.699793')
+# Convert back to BibEntry and write BibTeX file
+df2bib(CCRCN_bib, file = "./docs/bibliography/CCRCN_bibliography_out.bib")
 
 
 
-## Write BibTeX file
-WriteBib(bib, "./docs/biblio.bib", biblatex = TRUE)
 
 
