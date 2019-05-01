959680fe7f3bf92be9d61262c5fabba97904c1ec
David <klingesd@si.edu>
2019-05-01 08:47:52 -0400

    in-progress biblio compilation, need to revise .bib citations before moving forward

--- scripts/lit_compiler_helpers/biblio_compilation.R
+++ scripts/lit_compiler_helpers/biblio_compilation.R
@@ -9,23 +9,32 @@
 library(RefManageR)
 library(fuzzyjoin)
 library(tidyverse)
+library(bib2df)
+
+# Import CCRCN core data
+CCRCN_coredata <- read_csv("./data/CCRCN_synthesis/CCRCN_core_data.csv")
+
+# Import the CCRCN bibliography 
+CCRCN_bib <- bib2df("./docs/CCRCN_bibliography.bib")
 
 ## Curate Bibliography ###############
 
-# import the CCRCN bibliography 
-CCRCN_bib <- bib2df("./docs/bibliography/CCRCN_bibliography.bib")
 
 study_ids <- CCRCN_coredata %>%
   distinct(study_id) %>%
   select(study_id) %>%
   mutate(temp_id = study_id) %>%
-  mutate(author_year = substr("_", "_")) %>%
   separate(temp_id, into = c("author", "rest"),  sep = "_") %>%
-  mutate(author = gsub(pattern = '([[:upper:]])', perl = TRUE, replacement = '\\L\\1', author))
+  mutate(year = gsub("\\D", "", study_id)) %>%
+  mutate(author = gsub(pattern = '([[:upper:]])', perl = TRUE, replacement = '\\L\\1', author)) %>%
+  unite(author_year, c(author, year), sep = "")
 
 # fuzzyjoin
-CCRCN_bib_df <- fuzzy_join(CCRCN_bib, study_ids, by = c("BIBTEXKEY" = "author"), 
+CCRCN_bib_df <- fuzzy_join(CCRCN_bib, study_ids, by = c("BIBTEXKEY" = "author_year"), 
                         match_fun = str_match, mode = "full")
+
+# fuzzyjoi
+CCRCN_bib_df <- regex_inner_join(CCRCN_bib, study_ids, by = c(BIBTEXKEY = "author_year"))
 
 # Create a df of just study_id and BIBTEXKEY to link bib entries
 CCRCN_bib_link <- CCRCN_bib_df %>%
