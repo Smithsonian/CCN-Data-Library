4daa44360d3062f261d72e74b2ecfc98afa5a5ee
David <klingesd@si.edu>
2019-05-09 15:36:37 -0400

    improvements to library growth curve graphic
    still need to automate figure generation

--- scripts/1_data_formatting/library_growth.R
+++ scripts/1_data_formatting/library_growth.R
@@ -10,6 +10,11 @@
 
 library(tidyverse)
 library(taskscheduleR)
+library(scales)
+library(ggnewscale)
+library(extrafont)
+font_import()
+loadfonts(device = "win")
 
 
 ## Compile RCN library size metrics ################
@@ -22,13 +27,17 @@ library(taskscheduleR)
 growth_metrics <- tibble(
   date = as.Date(c("2018-06-01", "2018-07-01", "2018-08-01", "2018-09-01", 
                  "2018-10-01", "2018-11-01", "2018-12-01", "2019-01-01",
-                 "2019-02-01", "2019-03-01", "2019-04-01")),
-  total_cores = c(1535, 1535, 1535, 1535, 1546, 1546, 2555, 4025, 4263, 4276, 4571),
-  study_IDs = c(32, 32, 32, 32, 33, 33, 34, 188, 190, 192, 194),
+                 "2019-02-01", "2019-03-01", "2019-04-01", "2019-05-01")),
+  total_cores = c(1535, 1535, 1535, 1535, 1546, 1546, 2555, 4025, 4263, 4276, 
+                  4571, 5487),
+  study_IDs = c(32, 32, 32, 32, 33, 33, 34, 188, 190, 192, 194, 254),
   # Dated cores are inclusive of WG repo-only datasets, which can't be tapped into
   #   from this script
-  dated_cores = c(0, 0, 0, 0, 11, 11, 22, 22, 66, 70, 133)
+  dated_cores = c(0, 0, 0, 0, 11, 11, 22, 22, 66, 70, 133, 150)
 )
+
+# Read in first year of data 
+growth_metrics <- read_csv("data/library_metrics/original/growth_metrics.csv")
 
 # NOTE: 
 CCRCN_depthseries <- read_csv("./data/CCRCN_synthesis/CCRCN_depthseries_data.csv",
@@ -43,6 +52,7 @@ CCRCN_depthseries <- read_csv("./data/CCRCN_synthesis/CCRCN_depthseries_data.csv
                                                CD_reported = col_logical()
                                                ))
 
+CCRCN_depthseries <- read_csv("./data/CCRCN_synthesis/CCRCN_depthseries_data.csv")
 stop_for_problems(CCRCN_depthseries)
 
 
@@ -52,8 +62,48 @@ CCRCN_depthseries_dated <- CCRCN_depthseries %>%
 
 length(unique(CCRCN_depthseries_dated$core_id))
 
+length(unique(CCRCN_depthseries$study_id))
+
 
 ## Visualize growth #####################
 
 ggplot(growth_metrics, aes(date, total_cores)) +
-  geom_step()
+  # geom_step() +
+  geom_col() +
+  scale_fill_gradient2(low = '#003300', high = '#990000', mid = '#eae43f',
+                       midpoint = 3000, limits = c(0, 6000), oob = squish) +
+  theme_classic() + 
+  xlab("Date") +
+  ylab("Size of Library (# Cores")
+
+
+
+
+ggplot(growth_metrics, aes(x = date, y = total_cores,
+             fill = total_cores)) +
+  geom_col() +
+  # Set range of color scale for bars
+  # scale_color_gradient(low = "#56B1F7", high = "#132B43") +
+  
+  coord_cartesian(ylim = c(0, 6000)) + 
+  
+  # new_scale("fill") +
+  geom_col(data = growth_metrics, aes(x = date, y = dated_cores, fill = dated_cores)) +
+  # scale_fill_gradient2(low = "#1B7837", high = "#762A83") +
+  theme_classic() +
+  scale_x_date(labels = date_format("%Y-%m"), date_breaks = "2 months") +
+  theme(
+    # Remove undesired elements
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.line.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.position = "none",
+
+    # NOTE: this plot will fail if the Tunga font family is not installed
+    # axis.text.x = element_text(family = "Tunga", face = "bold", size = 12),
+    # axis.text.y = element_text(family = "Tunga", face = "bold", size = 14),
+    aspect.ratio = .35)
+
+
