2c047c0abdc5ac3af2a1753416ddf18aff89d64d
jaxinewolfe <wolfejax@si.edu>
2020-03-23 17:06:43 -0400

    updating sites and methods

--- scripts/0_data_hooks/Nahlik_and_Fennessey_2017_hook.R
+++ scripts/0_data_hooks/Nahlik_and_Fennessey_2017_hook.R
@@ -9,8 +9,8 @@ library(lubridate)
 # Download data ####
 
 # scrape data from web
-# condition <- read_csv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_cond_stress.csv")
-# condition_meta <- read_tsv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_cond_stress-meta.txt")
+condition <- read_csv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_cond_stress.csv")
+condition_meta <- read_tsv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_cond_stress-meta.txt")
 
 # site information
 siteinfo <- read_csv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_siteinfo.csv")
@@ -24,6 +24,8 @@ soilchem_meta <- read_tsv("https://www.epa.gov/sites/production/files/2016-10/nw
 plants <- read_csv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_plant_pres_cvr.csv")
 plants_meta <- read_tsv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_plant_pres_cvr-meta.txt")
 
+usaram <- read_csv("https://www.epa.gov/sites/production/files/2016-10/nwca2011_usaram_attributes.csv")
+
 # write original data
 write_csv(soilchem, "./data/primary_studies/Nahlik_Fennessey_2017/original/nwca2011_soilchem.csv")
 write_csv(siteinfo, "./data/primary_studies/Nahlik_Fennessey_2017/original/nwca2011_siteinfo-meta.csv")
@@ -36,14 +38,31 @@ site_marine <- siteinfo %>%
   filter(SANDT_RSTUDY == "Coastal Watersheds" &
            !is.na(AA_CENTER_LAT)) %>%
   select(SITE_ID, DATE_COL, AA_CENTER_LAT, AA_CENTER_LON, ANALYSIS_LAT, ANALYSIS_LON,
-         COE_REGION, CLASS_FIELD_HGM, HUC10_NAME, MAJ_RIVER_BASIN, SANDT_RSTUDY, SANDT_COAST_REG) %>%
+         COE_REGION, CLASS_FIELD_FWSST, CLASS_FIELD_HGM, HUC10_NAME, MAJ_RIVER_BASIN, 
+         SANDT_RSTUDY, SANDT_COAST_REG) %>%
+  mutate(salinity_class = recode(CLASS_FIELD_FWSST,
+                                 "E2EM" = "estuarine",
+                                 "E2SS" = "estuarine",
+                                 "PEM" = "palustrine",
+                                 "PFO" = "palustrine",
+                                 "PSS" = "palustrine",
+                                 "PF" = "palustrine",
+                                 "PUBPAB" = "palustrine"),
+         vegetation_class = recode(CLASS_FIELD_FWSST,
+                                   "E2EM" = "emergent",
+                                   "E2SS" = "forested to shrub",
+                                   "PEM" = "emergent",
+                                   "PFO" = "forested",
+                                   "PSS" = "scrub shrub",
+                                   "PF" = NA_character_,
+                                   "PUBPAB" = NA_character_)) %>%
   rename(site_id = SITE_ID)
 
 site_marine$DATE_COL <- dmy(site_marine$DATE_COL)  
 
 # Cores/Depthseries? ####
 soil_marine <- soilchem %>%
-  filter(SITE_ID %in% unique(site_marine$SITE_ID)) %>%
+  filter(SITE_ID %in% unique(site_marine$site_id)) %>%
   select(SITE_ID, DATE_COL, SAMPLE_ID, DEPTH, DEPTH_FLAG, TOT_CARBON, EC) %>%
   rename(site_id = SITE_ID, 
          core_date = DATE_COL,
@@ -53,15 +72,30 @@ soil_marine$core_date <- as.Date(soil_marine$core_date, format = "%m/%d/%Y")
 
 # Species ####
 species_marine <- plants %>%
-  filter(SITE_ID %in% unique(site_marine$SITE_ID)) %>%
-  select(SITE_ID, DATE_COL, COVER, SPECIES, SPECIES_NAME_ID, SPECIES_COMMENT) %>%
+  filter(SITE_ID %in% unique(site_marine$site_id) &
+           COVER > 20.0) %>%
+  select(SITE_ID, UID, DATE_COL, SPECIES, SPECIES_NAME_ID, SPECIES_COMMENT) %>%
   separate(col = SPECIES, into = c("genus", "species"), sep = " ") %>%
   mutate(species = replace_na(species, "spp.")) %>%
   rename(site_id = SITE_ID,
          species_code = SPECIES_NAME_ID,
-         species_notes = SPECIES_COMMENT)
+         species_notes = SPECIES_COMMENT) %>%
+  distinct()
 
 species_marine$DATE_COL <- dmy(species_marine$DATE_COL)
+
+# Impact ####
+
+
+
+# Methods ####
+methods <- data.frame(study_id = "Nahlik_and_Fennessey_2017",
+                      coring_method = "push core",
+                      sediment_sieve_size = "sediment sieved", 
+                      carbon_measured_or_modeled = "?",
+                      carbonates_removed = TRUE,
+                      carbonate_removal_method = "direct acid treatment",
+                      fraction_carbon_method = "EA")
 
 # Investigate Data ####
 # map the data
@@ -77,9 +111,11 @@ map <- ggplot() +
   coord_quickmap()
 
 # Points should all be coastal
-map + geom_point(data = site_marine, aes(x = AA_CENTER_LON, y = AA_CENTER_LAT, col = CLASS_FIELD_HGM)) +
+map + geom_point(data = site_marine, aes(x = ANALYSIS_LON, y = ANALYSIS_LAT, col = CLASS_FIELD_FWSST)) +
   theme_classic()
 
 map + geom_point(data = site_marine, aes(x = ANALYSIS_LON, y = ANALYSIS_LAT, col = CLASS_FIELD_HGM)) +
   theme_classic()
 
+map + geom_point(data = site_marine, aes(x = ANALYSIS_LON, y = ANALYSIS_LAT, col = salinity_class)) +
+  theme_classic()
