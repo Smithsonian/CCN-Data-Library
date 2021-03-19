beb1984e0ada4655a8c99f47983efc5e9fcd7f3e
jaxinewolfe <wolfejax@si.edu>
2021-03-19 16:16:31 -0400

    eliminated unnecessary shapefile and reordered the post-processing scripts

---
+++ scripts/3_post_processing/3_habitat_classification_cleanup.R
@@ -0,0 +1,108 @@
+# Coastal Carbon Research Coordination Network ####
+# Database Guidance V2
+
+# Synthesis Post-Processing Script
+# Assign habitat to each core in the synthesis
+# contact: James Holmquist (HolmquistJ@si.edu) or Jaxine Wolfe (wolfejax@si.edu)
+require(tidyverse)
+
+methods2 <- read_csv("data/CCRCN_V2/methods.csv") # methods
+cores2 <- read_csv("data/CCRCN_V2/cores.csv", guess_max = 7000)
+depthseries2 <- read_csv("data/CCRCN_V2/depthseries.csv", guess_max = 50000)
+species2 <- read_csv("data/CCRCN_V2/species.csv")
+
+# If habitat is defined explicitly, then it will be used
+if ("habitat" %in% names(cores2)) {
+  cores2 <- cores2 %>% rename(habitat1 = habitat)
+} else {
+  cores2 <- cores2 %>% mutate(habitat1 = NA_character_)
+}
+
+# First classify habitat according to the big habitat-specific syntheses
+sanderman <- read_csv("scripts/3_post_processing/tables/input_files/synthesis_studies/Sanderman_2018_cores.csv") %>% 
+  select(study_id, core_id) %>% 
+  mutate(habitat2 = "mangrove")
+
+forquean <- read_csv("scripts/3_post_processing/tables/input_files/synthesis_studies/Fourqurean_2012_cores.csv") %>% 
+  select(study_id, core_id) %>% 
+  mutate(habitat2 = "seagrass")
+
+two_big_syntheses <- bind_rows(sanderman, forquean)
+
+# Cores classified by salinity and vegetation 
+cores_sal_veg_habitat <- cores2 %>% 
+  # Good classification
+  mutate(habitat3 = ifelse(vegetation_class == "emergent", "marsh",
+                           ifelse(vegetation_class == "seagrass", "seagrass",
+                                  ifelse(vegetation_class %in% c("mudflat", "unvegetated"),
+                                         "unvegetated",
+                                         ifelse(vegetation_class == "scrub shrub", "scrub/shrub", NA)))),
+        # Kind of junk classification, but best we can do if none is better
+        habitat7 = ifelse(vegetation_class %in% c("forested", "forested to shrub", "forested to emergent") &
+                            salinity_class %in% c("saline", "brackish", "mesohaline",
+                                                        "brackish to fresh", "polyhaline", "estuarine C-CAP",
+                                                        "estuarine", "bracish to saline", "intermediate salinity"),
+                          ifelse(study_id == "Nahlik_and_Fennessy_2016", # If it's from Nhalick and fennesey and it's forrested then it's U.S.
+                                 ifelse(latitude <= 29.75, "mangrove", "swamp"), 
+                                 NA),  # Cavanaugh et al 2014, Use the max northern limit observed by Cavanaugh
+                          NA)) %>%
+  select(study_id:core_id, vegetation_class, salinity_class, habitat1, habitat3, habitat7)
+
+cores_species_habitat <- species2 %>% 
+  mutate(habiat = factor(habitat, levels = rev(c("unvegetated", "algal mat", "seagrass", "marsh", "scrub/shrub", "swamp", "mangrove")))) %>% 
+  arrange(study_id, core_id, habiat) %>% 
+  group_by(study_id, core_id, core_id) %>%
+  summarise(habitat4 = first(habitat)) %>% 
+  mutate(habitat4 = as.character(habitat4))
+
+habitat_comparison <- cores_sal_veg_habitat %>% full_join(two_big_syntheses, by = c("study_id", "core_id")) %>%
+  left_join(cores_species_habitat, by = c("study_id", "core_id")) %>% 
+  select(study_id, site_id, core_id, vegetation_class, salinity_class, habitat1, habitat2, habitat3, habitat4, habitat7)
+
+study_habitat_ids_manual <- read_csv("scripts/3_post_processing/tables/input_files/v1p2_to_v2/missing_habitat_manual_cleanup.csv")
+
+habitat_comparison <- habitat_comparison %>% 
+  left_join(study_habitat_ids_manual)
+
+habitat_comparison <- habitat_comparison %>% 
+  mutate(habitat6 = ifelse(grepl("mangrove", site_id) | grepl("mangrove", core_id), "mangrove",
+                           ifelse(grepl("swamp", site_id) | grepl("swamp", core_id) | grepl("forest", site_id) | grepl("forest", core_id), "swamp",
+                                  ifelse(grepl("marsh", site_id) | grepl("marsh", core_id), "marsh",
+                                         ifelse(grepl("seagrass", site_id) |  grepl("seagrass", core_id), "seagrass", NA)))))
+
+habitat_comparison_NA2 <- filter(habitat_comparison, is.na(habitat1) & is.na(habitat2) & is.na(habitat3) & is.na(habitat4) & is.na(habitat5) & is.na(habitat6) & is.na(habitat7))
+
+# Seems pretty reasonable to leave these NA's
+# StLaurent cores are probably marsh
+# there are some species described that havent been assicated with a habitat in our species-habitat table
+
+habitat_final <- habitat_comparison %>% 
+  select(study_id, core_id, habitat1:habitat6) %>%
+  gather(key="habitat_assignment_method",
+         value = "habitat", -c(study_id, core_id)) %>% 
+  arrange(study_id, core_id, habitat_assignment_method) %>% 
+  filter(complete.cases(.)) %>% 
+  group_by(study_id, core_id) %>% 
+  summarise(habitat = first(habitat),
+            habitat_assignment_method = first(habitat_assignment_method)) %>% 
+  mutate(habitat_assignment_method = recode(habitat_assignment_method, 
+                                            "habitat1" = "habitat explicitly defined in original hook script",
+                                            "habitat2" = "study is from a habitat specific synthesis",
+                                            "habitat3" = "habitat determined from vegetation_class",
+                                            "habitat4" = "determined based on species presence",
+                                            "habitat5" = "study-level habitat manually coded by manager",
+                                            "habitat6" = "partial text matching used to detect habitat descriptions in site_id or core_id",
+                                            "habitat7" = "habitat determined from vegetation_class, salinity_class, and latitude for forested wetlands"))
+
+
+cores_with_habitat <- cores2 %>% 
+  left_join(habitat_final, by=c("study_id", "core_id")) %>%
+  select(-habitat1) # Keshta habitat is defined in the hook script
+
+# write to file!
+write_csv(cores_with_habitat, "data/CCRCN_V2/cores.csv")
+
+# ggplot(data = cores_with_habitat, aes(x=longitude, y=latitude)) +
+#   geom_point(aes(color=habitat), alpha=0.4) +
+#   theme_dark()
+
---
+++ scripts/3_post_processing/6_core_attributes.R
@@ -0,0 +1,112 @@
+# Coastal Carbon Research Coordination Network
+# Database Guidance V2
+# Synthesis Post-Processing Script
+
+# Contact: Jaxine Wolfe, wolfejax@si.edu
+
+# The code iterates through the CCN database, which has already been converted to V2 guidance
+# It looks at the data available for each core and extracts the attributes names for which data is not entirely missing.
+# The final table will have the following columns: study_id, core_id, table, attribute_name
+
+library(tidyverse)
+
+# read in necessary data
+cores <- read_csv("data/CCRCN_V2/cores.csv", guess_max=6206)
+depthseries <- read_csv("data/CCRCN_V2/depthseries.csv", guess_max = 42698)
+
+## Step 1: Get unique ids for study, site, and core ####
+
+if(!(file.exists("data/CCRCN_V2/core_attributes.csv"))){
+  # this runs the script for all cores in the synthesis
+  # takes a few minutes
+  study_site_core <- cores %>% 
+    select(study_id, site_id, core_id)
+  
+  update_existing_table <- FALSE
+  
+} else {
+
+  # read in current version of the core attributes table
+  current_core_attributes <-  read_csv("data/CCRCN_V2/core_attributes.csv")
+  
+  # filter core table for new core ids
+  # Get unique ids for study, site, and core
+  study_site_core <- cores %>% 
+    filter(!(core_id %in% current_core_attributes$core_id)) %>%
+    select(study_id, site_id, core_id)
+  
+  update_existing_table <- TRUE
+}
+
+## Step 2: Extract core attributes at the core and depthseries level ####
+
+if(!(plyr::empty(study_site_core))){
+  
+  # create a table to store the result
+  all_core_attributes <- data.frame()
+  absent_cores <- data.frame()
+  
+  # Iterate through study/site/core ID's
+  for (i in 1:nrow(study_site_core)) {
+    
+    # Store the study and core id
+    temp_study_id <- study_site_core$study_id[i]
+    # temp_site_id <- study_site_core$site_id[i] # unneccessary
+    temp_core_id <- study_site_core$core_id[i]
+    
+    # Extract attribute names one core at a time for the core and depthseries tables
+    temp_core <- cores %>% filter(study_id == temp_study_id & core_id == temp_core_id) %>%
+      select_if(function(x) {!all(is.na(x))}) %>% # Remove columns that are entirely NA
+      mutate_all(as.character) %>%
+      # gather attribute names into one column
+      pivot_longer(-c(study_id, core_id), names_to = "attribute_name", values_to = "data") %>%
+      mutate(table = "core_level") %>%
+      select(-data)
+    
+    if(!(temp_core_id %in% depthseries$core_id)){
+      
+      # aggregate the attributes for the cores table
+      all_core_attributes <- bind_rows(all_core_attributes, temp_core) %>% distinct()
+      
+      # document cases where a core id is not in the depthseries table
+      # Currently, 11 Peck_et_al_2020 cores are not in the depthseries table
+      absent_cores <- data.frame(study_id = temp_study_id, core_id = temp_core_id) %>% 
+        bind_rows(absent_cores)
+      
+      print(paste0("Core is not in depthseries table: ", temp_core_id))
+      
+    } else {
+      
+      temp_depthseries <- depthseries %>% 
+        filter(study_id == temp_study_id & core_id == temp_core_id) %>%
+        select_if(function(x) {!all(is.na(x))}) %>% # Remove columns that are entirely NA
+        mutate_all(as.character) %>%
+        # gather attribute names into one column
+        pivot_longer(-c(study_id, core_id), names_to = "attribute_name", values_to = "data") %>%
+        mutate(table = "depthseries") %>%
+        select(-data)
+      
+      # create a df with unique attributes for each core 
+      core_attributes <- bind_rows(temp_core, temp_depthseries) %>% distinct()
+      
+      # aggregate the attributes for the cores and depthseries tables
+      all_core_attributes <- bind_rows(all_core_attributes, core_attributes)
+      
+    }
+    
+  }
+  
+  if(update_existing_table == TRUE){
+    # add new cores to the existant core attribute table
+    all_core_attributes <- bind_rows(current_core_attributes, all_core_attributes)
+  }
+  
+  # write data to CCRCN_V2 folder
+  write_csv(all_core_attributes, "data/CCRCN_V2/core_attributes.csv")
+  
+} else {
+  
+  print("There are no new cores to process.")
+}
+
+
