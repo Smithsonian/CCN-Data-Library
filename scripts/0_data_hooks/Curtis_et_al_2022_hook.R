c9d92f3d9f1a93e7f7b1474841f353221f6b5f3e
henry5184 <106403659+BettsH@users.noreply.github.com>
2022-10-17 11:03:52 -0400

    Curtis et al 2022 and Rodriguez et al 2022 hooks
    First draft of the Curtis hook and second round edits to the Rodriguez hook

---
+++ scripts/0_data_hooks/Curtis_et_al_2022_hook.R
@@ -0,0 +1,157 @@
+## CCN Data Library ####
+
+## Soil core data curation script for Curtis et al 2022
+## contact: Henry Betts, BettsH@si.edu
+
+## DOI: https://doi.org/10.5066/P9QLAL7B
+## Associated article: A Summary of Water-Quality and Salt Marsh Monitoring, Humboldt Bay, California
+## Article URL: https://pubs.er.usgs.gov/publication/ofr20221076/full
+
+# load necessary libraries
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(RefManageR)
+library(sf)
+library(leaflet)
+library(skimr)
+
+# load in helper functions
+source("scripts/1_data_formatting/curation_functions.R") # For curation
+source("scripts/1_data_formatting/qa_functions.R") # For QAQC
+
+# link to database guidance for easy reference:
+# https://smithsonian.github.io/CCRCN-Community-Resources/soil_carbon_guidance.html
+
+
+## Step 1: Setting up the datasets ####
+
+methods_raw <- read_excel("./data/primary_studies/Curtis_et_al_2022/intermediate/curtis_et_al_2022_methods.xlsx", sheet = 2)
+cores_raw <- read_csv("./data/primary_studies/Curtis_et_al_2022/original/Summary_of_carbon_storage_in_five_salt_marshes_Humboldt_Bay_CA_2018.csv")
+depthseries_raw <- read_csv("./data/primary_studies/Curtis_et_al_2022/original/Sediment_characteristics_of_five_salt_marshes_Humboldt_Bay_CA_2018.csv")
+
+## Depthseries
+depthseries <- depthseries_raw %>% 
+  separate(col = `Sample ID`, into = "core_id", sep = '-', remove = FALSE) %>% 
+  mutate(study_id = "Curtis_et_al_2022",
+         site_id = substr(core_id, 1, 3),
+         sample_id = gsub("-", "_", `Sample ID`),
+         depth_min = `Section Bottom Depth (cm)` - 10,
+         method_id = 'single set of methods',
+         marker_type = "artificial horizon",
+         method_id = "single set of methods",
+         compaction_notes = "little to no compaction because once corer is inserted, the core is rotated 180 degrees, and a sample is collected adjacent to where it was driven into the ground") %>% 
+  rename(depth_max = `Section Bottom Depth (cm)`,
+         dry_bulk_density = `Dry Bulk Density (g cm-3)`,
+         fraction_organic_matter = `Fraction OC`) %>% 
+  select(c(study_id, site_id, core_id, method_id, depth_min, depth_max, sample_id, dry_bulk_density, fraction_organic_matter, 
+           marker_type))
+
+## Cores
+cores <- cores_raw %>% 
+  rename(core_id = `Core ID`,
+         easting = `Easting (meters)`,
+         northing = `Northing (meters)`,
+         elevation = `Elevation (meters)`) %>% 
+  mutate(study_id = 'Curtis_et_al_2022',
+         site_id = substr(core_id, 1, 3),
+         day = day(as.Date(Date, "%m/%d/%Y")),
+         month = month(as.Date(Date, "%m/%d/%Y")),
+         year = year(as.Date(Date, "%m/%d/%Y")),
+         zone = 10,
+         salinity_class = "estuarine",
+         salinity_method = "measurement",
+         habitat = "marsh",
+         core_length_flag = ifelse(core_id == 'JSF1', "core depth represents deposit depth", "core depth limited by length of corer"),
+         core_id = ifelse(core_id == 'JSF1', "JSF2", core_id)) %>%  # core_id JSF1 in this dataset is named JSF2 in depthseries; correcting the name here
+  UTM_to_DD() %>% 
+  select(c(study_id, site_id, core_id, year, month, day, latitude, longitude, elevation, salinity_class, salinity_method, 
+           habitat, core_length_flag))
+
+## Methods
+methods <- methods_raw %>% 
+  select(c(study_id, method_id, compaction_flag, dry_bulk_density_temperature, dry_bulk_density_time, dry_bulk_density_sample_volume, 
+              dry_bulk_density_flag, carbon_measured_or_modeled, carbonates_removed, carbonate_removal_method, fraction_carbon_method, 
+              fraction_carbon_type, carbon_profile_notes))
+
+## Impacts
+impacts <- cores[, c("study_id", "site_id", "core_id")] %>% 
+  mutate(impact_class = "natural")
+
+
+## Step 2: QAQC ####
+
+## Mapping
+leaflet(cores) %>%
+  addTiles() %>% 
+  addCircleMarkers(lng = ~longitude, lat = ~latitude, radius = 3, label = ~core_id)
+
+## Table testing
+table_names <- c("cores", "depthseries", "methods", "impacts")
+
+# Check col and varnames
+testTableCols(table_names)
+testTableVars(table_names)
+
+# test required and conditional attributes
+testRequired(table_names)
+testConditional(table_names)
+
+# test uniqueness
+testUniqueCores(cores)
+testUniqueCoords(cores)
+
+# test relational structure of data tables
+testIDs(cores, depthseries, by = "site")
+testIDs(cores, depthseries, by = "site")
+
+# test numeric attribute ranges
+fractionNotPercent(depthseries)
+testNumericCols(depthseries)
+
+
+## Step 3: Writing Curated Data ####
+write_csv(cores, "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_cores.csv") 
+write_csv(depthseries, "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_depthseries.csv")
+write_csv(methods, "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_methods.csv")
+write_csv(impacts, "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_impacts.csv")
+
+
+## Step 4: Bibliography ####
+citation_article <- data.frame(study_id = "Curtis_et_al_2022",
+                               bibliography_id = "Curtis_et_al_2022_article",
+                               title = "A Summary of Water-Quality and Salt Marsh Monitoring, Humboldt Bay, California",
+                               author = "Jennifer A. Curtis, Karen M. Thorne, Chase M. Freeman, Kevin J. Buffington, and Judith Z. Drexler",
+                               doi = "10.3133/ofr20221076",
+                               url = "https://doi.org/10.3133/ofr20221076", 
+                               bibtype = "Article",
+                               journal = "USGS Publications Warehouse",
+                               publication_type = 'associated source',
+                               year = "2022",
+                               month = "sep",
+                               day = '16')
+
+citation_data <- data.frame(study_id = "Curtis_et_al_2022",
+                            bibliography_id = "Curtis_et_al_2022_data",
+                            title = "Salt marsh monitoring during water years 2013 to 2019, Humboldt Bay, CA – water levels, surface deposition, elevation change, and carbon storage",
+                            author = "Curtis, J.A., Thorne, K.M., Freeman C.M., Buffington, K.J., and Drexler, J.Z.",
+                            publication_type = "primary dataset",
+                            doi = "10.5066/P9QLAL7B",
+                            url = 'https://doi.org/10.5066/P9QLAL7B',
+                            bibtype = "Misc",
+                            year = "2022",
+                            month = "jun",
+                            day = '23')
+
+study_citations <- bind_rows(citation_article, citation_data)
+
+bib_file <- study_citations %>%
+  remove_rownames() %>% 
+  select(-c(study_id, publication_type)) %>% 
+  column_to_rownames("bibliography_id")
+
+WriteBib(as.BibEntry(bib_file), "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_study_citations.bib")
+write_csv(study_citations, "data/primary_studies/Curtis_et_al_2022/derivative/Curtis_et_al_2022_study_citations.csv")
+
+
+
--- scripts/0_data_hooks/Rodriguez_et_al_2022_hook.R
+++ scripts/0_data_hooks/Rodriguez_et_al_2022_hook.R
@@ -30,8 +30,6 @@ depthseries_raw <- read_xlsx("./data/primary_studies/Rodriguez_et_al_2022/origin
 species_raw <- read_csv("./data/primary_studies/Rodriguez_et_al_2022/intermediate/Table_1.csv", skip = 1) # this is the converted .csv file (.../Rodriguez_et_al_2022/intermediate) made from the original .docx (.../Rodriguez_et_al_2022/original)
 methods_raw <- read_csv("./data/primary_studies/Rodriguez_et_al_2022/intermediate/Methods.csv")
 
-# Note that each of the following data frame set ups need to be run as a whole because variables are sequentially edited
-
 ## Cores
 cores_set_up <- cores_raw %>% 
   fill(Site, `Core Number`) %>% 
@@ -40,8 +38,13 @@ cores_set_up <- cores_raw %>%
          elevation_cores = `Elevation (m; NAVD88)`,
          c14_age = 'Radiocarbon Age (yr BP)',
          c14_age_se = 'Age Error') %>% 
+<<<<<<< Updated upstream
   unite(col = "core_id", site_id : core_id, sep = '_', remove = FALSE) %>% 
   distinct(core_id, .keep_all = TRUE) %>% 
+=======
+  unite(col = core_id, site_id : core_id, sep = '_', remove = FALSE) %>% 
+  distinct(core_id, .keep_all = TRUE) %>% # this removes all of the duplicate rows due to multiple calibrated age ranges per core ID
+>>>>>>> Stashed changes
   mutate(core_id = str_replace_all(string = core_id, pattern = '-', replacement = '_'),
          site_id = str_replace_all(string = site_id, pattern = '-', replacement = '_'),
          study_id = "Rodriguez_et_al_2022",
@@ -54,9 +57,14 @@ cores_set_up <- cores_raw %>%
          habitat = "marsh",
          salinity_class = 'estuarine',
          inundation_method = 'field observation',
-         inundation_class = ifelse(site_id %in% c("SRE_U", "SRE_D", "NPR_D"), "low", "high"))
+         inundation_class = ifelse(site_id %in% c("SRE_U", "SRE_D", "NPR_D"), "low", "high"),
+         position_method = "RTK",
+         elevation_accuracy = "±.02–.03",
+         position_accuracy = "<.01",
+         c14_age_se = na_if(c14_age_se, 'n/a'),
+         c14_age = na_if(c14_age, 'n/a'),
+         c14_material = ifelse(is.na(c14_age), "NA", "plant fragment"))
   
-
 ## Species
 species_set_up <- species_raw %>% 
   # species_raw[-c(1, 24:26), ] %>% # eliminating metadata rows
@@ -67,7 +75,8 @@ species_set_up <- species_raw %>%
          thickness = "Thickness \n(m)", age = "Date \n(CE) a", stock = "C Stock (g \nm-2) b") %>% 
   separate(col = core_id, into = "site_id", sep = ";", remove = FALSE) %>% 
   mutate(study_id = "Rodriguez_et_al_2022",
-         code_type = 'genus',
+         code_type = 'Genus',
+         species_code = paste(species_code, "", sep = ' sp.'),
          habitat = 'marsh',
          core_id = gsub('; ', '_', core_id),
          core_id = str_replace_all(string = core_id, pattern = '-', replacement = '_'),
@@ -75,8 +84,7 @@ species_set_up <- species_raw %>%
          site_id = str_replace_all(string = site_id, pattern = '-', replacement = '_'),
          zone = ifelse(site_id == "SRE_U", 17,
                        ifelse(site_id == "SRE_D", 17, 18))) %>% 
-  UTM_to_DD(core_id = core_id, easting = easting, northing = northing, zone = zone)
-
+  UTM_to_DD()
 
 ## Depthseries
 depthseries_set_up <- depthseries_raw %>% 
@@ -85,26 +93,30 @@ depthseries_set_up <- depthseries_raw %>%
          dry_bulk_density = `Dry Bulk Density (g cm-3)`) %>% 
   mutate(study_id = "Rodriguez_et_al_2022", 
          fraction_organic_matter = `LOI (%)`/100,
-         fraction_carbon = `Carbon (%)`/100) %>% 
-  select(-c(`Dry mass (g)`,`Ignited mass (g)`,`LOI (%)`,`Carbon (%)`, `g C m-2`, `± g C m-2\r\n(95% CI)`)) %>% 
+         fraction_carbon = `Carbon (%)`/100,
+         method_id = "single set of methods") %>% 
   separate(col = core_id, into = c("site_id_1", "site_id_2", "site_id_3"), sep = "-", remove = FALSE) %>% 
 # there will be a warning message of missing pieces: this is OK as not all core_id data have 2 "-"; corrected below
   mutate(site_id = ifelse(is.na(site_id_3), site_id_1, paste(site_id_1, site_id_2, sep = "_")),
+<<<<<<< Updated upstream
          core_id = str_replace_all(string = core_id, pattern = '-', replacement = '_'))
+=======
+         core_id = str_replace_all(string = core_id, pattern = '-', replacement = '_')) 
+>>>>>>> Stashed changes
 
 
-## Step 2: Adding/subtracting required columns from data frame set ups to add to final data frames ####
+## Step 2: Merging and trimming to create final data frames ####
 
 # Adding lat., long., & year from 'species' to 'cores'
 cores <- full_join(cores_set_up, species_set_up) %>% 
-  select(-c(`Collection Interval (cm)`, `Fraction Modern`, 'FM Error',c14_age, c14_age_se, `Calibrated Age Range (CE)`, 
-            Certainty, `Median Probability`, `Comments:`, species_code, development_mode, elevation_species, thickness, 
-            age, stock, code_type, easting, northing, zone)) %>% 
+  select(c(study_id, site_id, core_id, year, latitude, longitude, position_accuracy, position_method, elevation_cores, 
+           elevation_datum, elevation_method, salinity_class, salinity_method, vegetation_class, vegetation_method, 
+           habitat, inundation_class, inundation_method, core_length_flag)) %>%  
   rename(elevation = elevation_cores)
-
 
 # Adding c14 age from 'cores' to 'depthseries'  
 depthseries <- full_join(depthseries_set_up, cores_set_up) %>% 
+<<<<<<< Updated upstream
   select(-c(site_id_1, site_id_2, site_id_3, elevation_cores, 'Collection Interval (cm)', 'Fraction Modern', 'FM Error', 
             'Calibrated Age Range (CE)', 'Certainty', 'Median Probability', 'Comments:', elevation_datum, elevation_method, 
             salinity_class, salinity_method, core_length_flag, vegetation_class, vegetation_method, habitat, inundation_method,
@@ -119,6 +131,21 @@ methods <- methods_raw %>% select(where(notAllNA))
   #                                 loss_on_ignition_sample_mass, loss_on_ignition_sample_volume, carbon_profile_notes, 
   #                                 cs137_counting_method, pb210_counting_method, excess_pb210_rate, excess_pb210_model, 
   #                                 ra226_assumption, age_depth_model_notes))
+=======
+  select(c(study_id, site_id, core_id, method_id, depth_min, depth_max, dry_bulk_density, fraction_organic_matter, 
+           fraction_carbon, c14_age, c14_age_se, c14_material))
+    
+# Trimming species data
+species <- select(species_set_up, c(study_id, site_id, core_id, species_code, code_type, habitat))
+     
+# Trimming methods data
+methods <- methods_raw %>% 
+  select(c(study_id, method_id, coring_method, sediment_sieved_flag, sediment_sieve_size, compaction_flag, dry_bulk_density_temperature, 
+           dry_bulk_density_time, dry_bulk_density_flag, loss_on_ignition_temperature, loss_on_ignition_time, loss_on_ignition_flag, 
+           carbon_measured_or_modeled, carbonates_removed, carbonate_removal_method, fraction_carbon_method, fraction_carbon_type, 
+           c14_counting_method, dating_notes, age_depth_model_reference))
+    
+>>>>>>> Stashed changes
 
 ## Step 3: QAQC ####
 
@@ -128,7 +155,7 @@ leaflet(cores) %>%
   addCircleMarkers(lng = ~longitude, lat = ~latitude, radius = 3, label = ~core_id)
 
 ## Table testing
-table_names <- c("cores", "depthseries", "species")
+table_names <- c("cores", "depthseries", "species", "methods")
 
 # Check col and varnames
 testTableCols(table_names)
@@ -149,6 +176,7 @@ testIDs(cores, depthseries, by = "site")
 # test numeric attribute ranges
 fractionNotPercent(depthseries)
 testNumericCols(depthseries)
+
 
 ## Step 4: Writing Curated Data ####
 
@@ -194,6 +222,3 @@ bib_file <- study_citations %>%
 
 WriteBib(as.BibEntry(bib_file), "data/primary_studies/Rodriguez_et_al_2022/derivative/Rodriguez_et_al_2022_study_citations.bib")
 write_csv(study_citations, "data/primary_studies/Rodriguez_et_al_2022/derivative/Rodriguez_et_al_2022_study_citations.csv")
-
-
-
