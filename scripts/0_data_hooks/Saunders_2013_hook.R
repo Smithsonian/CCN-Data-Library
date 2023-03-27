58cf366665c7d906a4cea84f7c5bd87f115375b5
henry5184 <106403659+BettsH@users.noreply.github.com>
2023-03-27 12:42:49 +0200

    CIFOR, McGlathery, Saunders, and Turck updates (all in progress)

---
+++ scripts/0_data_hooks/Saunders_2013_hook.R
@@ -0,0 +1,137 @@
+## CCN Data Library ####
+
+## Soil core data curation script for Saunders 2011
+## contact: Henry Betts, BettsH@si.edu
+## URL: https://doi.org/10.6073/pasta/c0cb8ff0f150e429674ecf0db15bedc5
+
+
+## 1. Set up ####
+
+# Load necessary libraries
+library(tidyverse)
+library(lubridate) # for as.Date()
+library(leaflet)
+library(RefManageR) # for bibliography
+
+# Load in helper functions
+source("scripts/1_data_formatting/curation_functions.R") # For curation
+source("scripts/1_data_formatting/qa_functions.R") # For QAQC
+
+# Link to database guidance for easy reference:
+# https://smithsonian.github.io/CCN-Community-Resources/soil_carbon_guidance.html
+
+# Read in data
+raw_data <- read.csv("./data/primary_studies/Saunders_2013/original/ST_OMD_Saunders_002")
+
+
+## 2. Organize tables ####
+depth_raw <- raw_data %>% 
+  rename(site_id = SITENAME,
+         core_id = Core_ID,
+         dry_bulk_density = BulkDensity,
+         excess_pb210_activity = Excess_Pb,
+         cs137_activity = X137Cs,
+         habitat = Habitat,
+         age = Year_210Pb_CF) %>% 
+  separate(SoilDepth_Interval, into = c("depth_min", "depth_max"), sep = "-") %>% 
+  mutate(dates = as.Date(Date),
+         year = year(dates),
+         month = month(dates),
+         day = day(dates),
+         method_id = "single set of methods",
+         study_id = "Saunders_2013",
+         site_id = gsub("-", "_", site_id),
+         core_id = paste(site_id, core_id, sep = "_"),
+         habitat = ifelse(habitat == "sawgrass", "seagrass", # EDIT: double check
+                          ifelse(habitat == "slough", "swamp", "NA")),
+         age = ifelse(grepl("-", age), "NA", age),
+         cs137_activity = ifelse(grepl("-", cs137_activity), "NA", cs137_activity),
+         cs137_unit = "picoCuriesPerGram",
+         pb210_unit = "numberPerGramPerMinute") 
+# EDIT: Cs137 unit? 
+
+depthseries <- depth_raw %>% 
+  select(study_id, site_id, core_id, method_id, depth_min, depth_max, dry_bulk_density, 
+         cs137_activity, cs137_unit, excess_pb210_activity, pb210_unit, age)
+         
+cores <- depth_raw %>% 
+  mutate(latitude = as.numeric(ifelse(site_id == "NE_SRS1", 25.698, 
+                           ifelse(site_id == "SRS3", 25.468,
+                                  ifelse(site_id == "SRS4", 25.410, "NA")))),
+         longitude = as.numeric(ifelse(site_id == "NE_SRS1", -80.635, 
+                            ifelse(site_id == "SRS3", -80.853,
+                                   ifelse(site_id == "SRS4", -80.964, "NA")))),
+         core_length_flag = "core depth limited by length of corer",
+         position_notes = "position listed at site level", # EDIT: position method?
+         vegetation_method = "field observation",
+         vegetation_class = ifelse(habitat == "seagrass", "seagrass", "other")) %>% # EDIT: double check
+  select(study_id, site_id, core_id, year, month, day, latitude, longitude, 
+         position_notes, vegetation_class, vegetation_method, habitat, core_length_flag) %>% 
+  distinct()
+
+methods <- data.frame(study_id = "Saunders_2013",
+                      method_id = "single set of methods",
+                      coring_method = "")
+# EDIT: coring method, position method, was anything removed/sieved/carbonates for DBD, DBD method, 
+## alpha/gamma Cs, excess pb model (crs, cic), salinity mentioned, core length flag
+
+
+## 3. QAQC ####
+
+## Mapping
+leaflet(cores) %>%
+  addTiles() %>% 
+  addCircleMarkers(lng = ~longitude, lat = ~latitude, radius = 3, label = ~core_id)
+
+## Table testing
+table_names <- c("cores", "depthseries", "methods")
+
+# Check col and varnames
+testTableCols(table_names)
+testTableVars(table_names)
+
+# test required and conditional attributes
+testRequired(table_names)
+testConditional(table_names)
+
+# test uniqueness
+testUniqueCores(cores)
+testUniqueCoords(cores)
+
+# test relational structure of data tables
+testIDs(cores, depthseries, by = "site")
+testIDs(cores, depthseries, by = "site")
+
+# test numeric attribute ranges
+fractionNotPercent(depthseries)
+testNumericCols(depthseries)
+
+
+## 4. Bibliography ####
+study_citation <- data.frame(study_id = "Saunders_2013",
+                             bibliography_id = "Saunders_2013_data",
+                             title = "Radiometric Characteristics of Soil Sediments from Shark River Slough, Everglades National Park (FCE) from 2005 and 2006",
+                             author = "Saunders, Colin",
+                             publication_type = "primary dataset",
+                             doi = "10.6073/pasta/c0cb8ff0f150e429674ecf0db15bedc5",
+                             url = "https://doi.org/10.6073/pasta/c0cb8ff0f150e429674ecf0db15bedc5",
+                             bibtype = "Misc",
+                             year = "2013")
+
+bib_file <- study_citation %>%
+  remove_rownames() %>% 
+  select(-c(study_id, publication_type)) %>% 
+  column_to_rownames("bibliography_id")
+
+
+## 5. Write curated data ####
+write_csv(cores, "./data/primary_studies/Saunders_2013/final/Saunders_2013_cores.csv") 
+write_csv(depthseries, "./data/primary_studies/Saunders_2013/final/Saunders_2013_depthseries.csv")
+write_csv(methods, "./data/primary_studies/Saunders_2013/final/Saunders_2013_methods.csv")
+WriteBib(as.BibEntry(bib_file), "./data/primary_studies/Saunders_2013/final/Saunders_2013_study_citations.bib")
+write_csv(study_citation, "./data/primary_studies/Saunders_2013/final/Saunders_2013_study_citations.csv")
+
+
+
+
+
---
+++ scripts/0_data_hooks/Turck_2014_hook.R
@@ -0,0 +1,161 @@
+## CCN Data Library ####
+
+## Soil core data curation script for Turck 2014
+## contact: Henry Betts, BettsH@si.edu
+## URL: https://doi.org/10.6073/pasta/4541ae084d807962b8c331eea61908bd
+
+
+## 1. Set up ####
+
+# Load necessary libraries
+library(tidyverse)
+library(leaflet)
+library(RefManageR)
+library(sf) # for UTM_to_DD()
+
+
+# Load in helper functions
+source("scripts/1_data_formatting/curation_functions.R") # For curation
+source("scripts/1_data_formatting/qa_functions.R") # For QAQC
+
+# Link to database guidance for easy reference:
+# https://smithsonian.github.io/CCN-Community-Resources/soil_carbon_guidance.html
+
+# Read in data
+cores_raw <- read.csv("./data/primary_studies/Turck_2014/original/GEL-GCES-1402_sampling_1_0.csv", skip = 2)
+depth_raw <- read.csv("./data/primary_studies/Turck_2014/original/GEL-GCES-1402_sediment_analysis_1_1.csv", skip = 2)
+
+
+## 2. Organize tables ####
+cores <- cores_raw[ , -1] %>% 
+  filter(grepl("MT", Name)) %>% 
+  rename(core_id = Name,
+         easting = East_NAD83,
+         northing = Nrth_NAD83,
+         species_code = Vegetation,
+         elevation = Elv_NAVD88) %>% 
+  mutate(study_id = "Turck_2014",
+         site_id = core_id,
+         elevation = as.numeric(elevation)/100,
+         position_method = "RTK",
+         position_accuracy = .1,
+         elevation_method = "RTK",
+         elevation_datum = "NAVD88",
+         elevation_accuracy = .2,
+         zone = 17,
+         vegetation_class = "seagrass",
+         vegetation_method = "field observation",
+         habitat = "marsh",
+         core_length_flag = "core depth represents deposit depth",
+         year = 2009) %>% 
+  UTM_to_DD() %>% 
+  select(study_id, site_id, core_id, year, latitude, longitude, position_accuracy, 
+         position_method, elevation, elevation_datum, elevation_accuracy, elevation_method, 
+         vegetation_class, vegetation_method, habitat, core_length_flag)
+
+species <- cores_raw %>% 
+  rename(species_code = Vegetation,
+         core_id = Name) %>% 
+  mutate(study_id = "Turck_2014",
+         site_id = core_id,
+         species_code = gsub("Short |Medium ", "", species_code),
+         code_type = "Genus",
+         habitat = "marsh") %>% 
+  select(study_id, site_id, core_id, species_code, code_type, habitat)
+
+# EDIT: change method ID?
+# EDIT: compaction at entire-core level. still list compaction fraction at e/depth interval (see cores_raw Sampl_lgth)?
+depthseries <- depth_raw %>% 
+  filter(grepl("MT", Name)) %>% 
+  rename(core_id = Name,
+         depth_min = Distance_Below_Ground_Surface,
+         fraction_organic_matter = Percent_Organic_Matter,
+         fraction_carbon = Total_Carbon) %>% 
+  mutate(study_id = "Turck_2014",
+         depth_max = as.numeric(depth_min) + 10,
+         site_id = core_id,
+         method_id = "single set of methods",
+         core_id = ifelse(grepl("extra", Distance_Above_MLLW), paste(core_id, "_2", sep = ""), core_id),
+         fraction_organic_matter = as.numeric(fraction_organic_matter)/100,
+         fraction_carbon = as.numeric(fraction_carbon)/100,
+         compaction_notes = "Compaction measured at the entire-core level") %>% 
+  select(study_id, site_id, core_id, method_id, depth_min, depth_max, fraction_organic_matter, fraction_carbon,
+         compaction_notes)
+
+methods <- data.frame(study_id = "Turck_2014",
+                      method_id = c("LOI_samples", "sieved_samples"), # EDIT: sieved only for grain analysis. include both?
+                      coring_method = "vibracore",
+                      roots_flag = "roots and rhizomes included",
+                      sediment_sieved_flag = "sediment sieved", # EDIT: or was just one method sieved
+                      sediment_sieve_size = .063,
+                      compaction_flag = "compaction quantified",
+                      loss_on_ignition_temperature = 1350,
+                      loss_on_ignition_time = "asked author", # EDIT: wait for response
+                      loss_on_ignition_flag = "not specified",
+                      carbon_measured_or_modeled = "measured", # EDIT: is this true if theres only FOM and no FOC?
+                      carbonates_removed = FALSE,
+                      carbonate_removal_method = "carbonates not removed",
+                      fraction_carbon_method = "not specified", # EDIT: is this the code if FOM was measured via LOI?
+                      fraction_carbon_type = "total carbon")
+
+
+## 3. QAQC ####
+
+## Mapping
+leaflet(cores) %>%
+  addTiles() %>% 
+  addCircleMarkers(lng = ~longitude, lat = ~latitude, radius = 3, label = ~core_id)
+
+## Table testing
+table_names <- c("cores", "depthseries", "methods", "species")
+
+# Check col and varnames
+testTableCols(table_names)
+testTableVars(table_names)
+
+# test required and conditional attributes
+testRequired(table_names)
+testConditional(table_names)
+
+# test uniqueness
+testUniqueCores(cores)
+testUniqueCoords(cores)
+
+# test relational structure of data tables
+testIDs(cores, depthseries, by = "site")
+testIDs(cores, depthseries, by = "site")
+
+# test numeric attribute ranges
+fractionNotPercent(depthseries)
+testNumericCols(depthseries)
+
+
+## 4. Bibliography ####
+study_citation <- data.frame(study_id = "Turck_2014",
+                             bibliography_id = "Turck_2014_data",
+                             title = "Vibracore and Tree Stump Data from the Marsh Near Mary Hammock, McIntosh County, GA",
+                             author = "Turck, John A",
+                             publication_type = "primary dataset",
+                             doi = "10.6073/pasta/4541ae084d807962b8c331eea61908bd",
+                             url = "https://doi.org/10.6073/pasta/4541ae084d807962b8c331eea61908bd",
+                             bibtype = "Misc",
+                             year = "2014")
+
+bib_file <- study_citation %>%
+  remove_rownames() %>% 
+  select(-c(study_id, publication_type)) %>% 
+  column_to_rownames("bibliography_id")
+
+
+## 5. Write curated data ####
+write_csv(cores, "./data/primary_studies/Turck_2014/final/Turck_2014_cores.csv") 
+write_csv(species, "./data/primary_studies/Turck_2014/final/Turck_2014_species.csv") 
+write_csv(depthseries, "./data/primary_studies/Turck_2014/final/Turck_2014_depthseries.csv")
+write_csv(methods, "./data/primary_studies/Turck_2014/final/Turck_2014_methods.csv")
+WriteBib(as.BibEntry(bib_file), "./data/primary_studies/Turck_2014/final/Turck_2014_study_citations.bib")
+write_csv(study_citation, "./data/primary_studies/Turck_2014/final/Turck_2014_study_citations.csv")
+
+
+
+
+
