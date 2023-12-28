447585cb06c550def637247c75809fefb3c95310
henry5184 <106403659+BettsH@users.noreply.github.com>
2023-12-28 14:20:05 +0100

    Langston et al 2022 hook

---
+++ data/primary_studies/Langston_et_al_2022/derivative/_readMe.txt
@@ -0,0 +1,16 @@
+Dataset: Dataset: The Effect of Marsh Age on Ecosystem Function in a Rapidly Transgressing Marsh
+
+Authors: Amy K. Langston, Daniel J. Coleman, Nathalie W. Jung, Justin L. Shawler, Alexander J. Smith, Bethany L. Williams, Serina S. Wittyngham, Randolph M. Chambers, James E. Perry, Matthew L. Kirwan
+
+The data release contains tidal wetland soil core information that can be joined across site ID and core ID.
+
+langston_et_al_2022_study_citations.bib - Contains bibliographic information for this data release
+langston_et_al_2022_study_citations.csv - Contains bibliographic information for this data release
+langston_et_al_2022_species.csv - Contains information on the dominant plant species at coring locations
+langston_et_al_2022_methods.csv - Contains information on materials and methods broken down by study 
+langston_et_al_2022_cores.csv - Contains positional and descriptive information on core locations
+langston_et_al_2022_depthseries.csv - Contains raw depth-series information for all cores
+
+metadata.xml - Contains a full suite of descriptive metadata with all attribute and variables defined, units clarified, and geographic, temporal, and taxonomic coverages described according to Ecological Metadata Language Standards.
+
+metadata.html - Is a simplified, visual and interactive version of metadata.xml for display purposes.
---
+++ scripts/0_data_hooks/Langston_et_al_2022_hook.R
@@ -0,0 +1,46 @@
+## CCN Data Library ####
+
+## Soil core data curation script for Langston et al 2022
+## contact: Henry Betts, BettsH@si.edu
+
+library(tidyverse)
+library(RefManageR)
+
+## Read files ####
+species <- read.csv("data/primary_studies/Langston_et_al_2022/original/langston_et_al_2022_species.csv")
+cores <- read.csv("data/primary_studies/Langston_et_al_2022/original/langston_et_al_2022_cores.csv")
+depthseries <- read.csv("data/primary_studies/Langston_et_al_2022/original/langston_et_al_2022_depthseries.csv") %>% 
+  select(!fraction_carbon)
+methods <- read.csv("data/primary_studies/Langston_et_al_2022/original/langston_et_al_2022_methods.csv")
+study_citations <- read.csv("data/primary_studies/Langston_et_al_2022/original/langston_et_al_2022_associated_publications.csv") %>% 
+  add_row(study_id = "Langston_et_al_2022", 
+          bibliography_id = "Langston_et_al_2022_data",
+          publication_type = "primary dataset", 
+          bibtype = "misc", 
+          title = "Dataset: The Effect of Marsh Age on Ecosystem Function in a Rapidly Transgressing Marsh",
+          author = "Langston, Amy K., Coleman, Daniel J., Jung, Nathalie W., Shawler,  Justin L., Smith, Alexander J., Williams, Bethany L., Wittyngham, Serina S., Chambers, Randolph M., Perry, James E., Kirwan, Matthew L.",
+          doi = "10.25573/serc.24913215", 
+          url = "https://smithsonian.figshare.com/articles/dataset/Dataset_The_Effect_of_Marsh_Age_on_Ecosystem_Function_in_a_Rapidly_Transgressing_Marsh/24913215", 
+          year = 2023,
+          month = NA,
+          publisher = "Smithsonian Environmental Research Center",
+          volume = NA,
+          pages = NA,
+          journal = NA,
+          copyright = "Creative Commons Attribution 4.0 International")
+
+
+bib_file <- study_citations %>%
+  remove_rownames() %>% 
+  select(-c(study_id, publication_type)) %>% 
+  column_to_rownames("bibliography_id")
+
+
+
+## Write files ####
+write.csv(cores, "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_cores.csv")
+write.csv(depthseries, "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_depthseries.csv")
+write.csv(methods, "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_methods.csv")
+write.csv(species, "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_species.csv")
+write.csv(study_citations, "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_study_citations.csv")
+WriteBib(as.BibEntry(bib_file), "./data/primary_studies/Langston_et_al_2022/derivative/langston_et_al_2022_study_citations.bib")
