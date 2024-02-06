8f82d3401a76ef13f70c8172522056d2dd439688
cheneyr <cheneyr@gwu.edu>
2024-02-06 10:00:35 -0500

    palinkas hook scripts
    Palinkas and Cornwell 2024, Palinkas and Engelhardt 2024

---
+++ data/primary_studies/Palinkas_and_Cornwell_2024/original/_readMe.txt
@@ -0,0 +1,16 @@
+Dataset:  A Preliminary Sediment Budget for the Corsica River (MD): Improved Estimates of Nitrogen Burial and Implications for Restoration
+
+Authors: Cindy M. Palinkas, Jeffrey Cornwell
+
+Any use of this dataset must include a citation. The DOI: 10.25573/serc.24467977
+
+The data release contains sediment carbon profiles from tidal wetlands. The data itself is housed in four separate .csv files which can by joined by the core_id and/or site_id attributes. 
+
+Palinkas_and_Cornwell_2024_methods.csv - Contains methodological information on the soil core collection and processing.
+Palinkas_and_Cornwell_2024_sites.csv - Contains site-level information.
+Palinkas_and_Cornwell_2024_depthseries.csv - Contains raw depth-series information for all cores.
+Palinkas_and_Cornwell_2024_cores.csv - Contains core, or sample-level information for all cores.
+
+metadata.xml - Contains a full suite of descriptive metadata with all attribute and variables defined, units clarified, and geographic, temporal, and taxonomic coverages described according to Ecological Metadata Language Standards.
+
+metadata.html - Is a simplified, visual and interactive version of metadata.xml for display purposes.
---
+++ scripts/0_data_hooks/Palinkas_and_Engelhardt_2024.R
@@ -0,0 +1,170 @@
+## CCN Data Library ########
+## contact: Rose Cheney, cheneyr@si.edu 
+
+## Soil core data curation script for Palinkas and Engelhardt 2024 dataset: Spatial and temporal patterns of modern sedimentation in a tidal freshwater marsh
+
+## Dataset: https://doi.org/10.25573/serc.24470152
+
+# load necessary libraries
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(RefManageR)
+library(leaflet)
+library(knitr)
+
+# load in helper functions
+source("scripts/1_data_formatting/curation_functions.R") # For curation
+source("scripts/1_data_formatting/qa_functions.R") # For QAQC
+
+
+# link to database guidance for easy reference:
+# https://smithsonian.github.io/CCRCN-Community-Resources/soil_carbon_guidance.html
+
+
+#load in data 
+methods_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_methods.csv")
+cores_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_cores.csv")
+depthseries_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_depthseries.csv")
+sites_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_sites.csv")
+species_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_species.csv")
+impacts_raw <- read_csv("data/primary_studies/Palinkas_and_Engelhardt_2024/original/Palinkas_and_Engelhardt_2024_impacts.csv")
+
+
+  
+## 1. Curation ####
+
+id <- "Palinkas_and_Engelhardt_2024"
+
+
+## ... Methods ####
+
+methods <- methods_raw %>% 
+  mutate(excess_pb210_rate = "cumulative mass")
+
+methods <- reorderColumns("methods", methods)
+
+
+## ... Sites ####
+
+sites <- sites_raw
+
+
+## ... Cores ####
+
+cores <- cores_raw %>% 
+  mutate(salinity_method = "field observation",
+         vegetation_method = "field observation") %>% 
+  select(-pb210_profile_max_age)# uncontrolled attribute
+
+
+cores <- reorderColumns("cores", cores)
+
+
+## ... Depthseries ####
+
+depthseries <- depthseries_raw %>% 
+  mutate(method_id = "single set of methods")
+depthseries <- reorderColumns("depthseries", depthseries)
+
+
+## ... Species ####
+
+species <- species_raw
+species <- reorderColumns("species", species)
+
+## ... Impacts ####
+
+impacts <- impacts_raw
+impacts <- reorderColumns("impacts", impacts)
+
+
+
+## 2. QAQC ####
+
+## Mapping
+leaflet(cores) %>%
+    addTiles() %>% 
+    addCircleMarkers(lng = ~longitude, lat = ~latitude, radius = 3, label = ~core_id)
+
+## Table testing
+table_names <- c("methods", "cores", "depthseries", "sites", "species", "impacts")
+
+# Check col and varnames
+testTableCols(table_names)
+testTableVars(table_names)
+
+# test required and conditional attributes
+testRequired(table_names)
+testConditional(table_names)
+
+# test uniqueness
+testUniqueCores(cores)
+testUniqueCoords(cores)
+
+# test relational structure of data tables
+testIDs(cores, depthseries, by = "site")
+testIDs(cores, depthseries, by = "core")
+
+# test numeric attribute ranges
+fractionNotPercent(depthseries)
+      #testNumericCols(depthseries)
+test_numeric_vars(depthseries) ##testNumericCols producing error message 
+
+## 3. Write Curated Data ####
+
+# write data to final folder
+write_csv(methods, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_materials_and_methods.csv")
+write_csv(cores, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_cores.csv")
+write_csv(sites,"data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_sites.csv")
+write_csv(depthseries, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_depthseries.csv")
+write_csv(impacts, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_impacts.csv")
+write_csv(species, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_species.csv")
+
+## 4. Bibliography ####
+
+# There are three ways to approach this:
+    # 1) download the article citation directly to the study's folder
+    # 2) create the study citation in the curation script and output it to the data release folder
+    # 3) create a study_citation table in an intermediate folder, read it in and output bib file to derivative folder
+
+# example study citation creation:
+# study_citation <- data.frame(bibliography_id = "Spera_et_al_2020",
+#                              title = "Spatial and temporal changes to a hydrologically-reconnected coastal wetland: Implications for restoration",
+#                              author = "Alina C. Spera and John R. White and Ron Corstanje",
+#                              bibtype = "Article",
+#                              doi = "10.1016/j.ecss.2020.106728",
+#                              url = "https://doi.org/10.1016/j.ecss.2020.106728", 
+#                              journal = "Estuarine, Coastal and Shelf Science",
+#                              year = "2020") %>% 
+#     column_to_rownames("bibliography_id")
+# 
+# WriteBib(as.BibEntry(study_citation), "data/primary_studies/Author_et_al_YYYY/derivative/Author_et_al_YYYY_associated_publications.bib")
+paper_citation <- dataset_citation <- data.frame(bibliography_id = "Palinkas_and_Engelhardt_2015",
+                                                 publication_type = "article",
+                                                 bibtype = "Article", 
+                                                 title = "Spatial and temporal patterns of modern (~100 yr) sedimentation in a tidal freshwater marsh: Implications for future sustainability",
+                                                 author = "Cindy M. Palinkas, Katharina A. M. Engelhardt",
+                                                 doi = "10.1002/lno.10202",
+                                                 url = "https://doi.org/10.1002/lno.10202",
+                                                 journal = "Limnology and Oceanography",
+                                                 year = "2015")
+
+
+
+dataset_citation <- data.frame(bibliography_id = id,
+                             publication_type = "primary dataset",
+                             bibtype = "Misc", 
+                             title = "Dataset: Spatial and temporal patterns of modern (~100 yr) sedimentation in a tidal freshwater marsh: Implications for future sustainability",
+                             author = "Cindy M. Palinkas, Katharina A. M. Engelhardt",
+                             doi = "10.25573/serc.24470152",
+                             url = "https://doi.org/10.25573/serc.24470152.v1",
+                             year = "2024")
+
+study_citations <- full_join(dataset_citation, paper_citation)
+
+WriteBib(as.BibEntry(study_citations), "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024.bib")
+write_csv(study_citations, "data/primary_studies/Palinkas_and_Engelhardt_2024/derivative/Palinkas_and_Engelhardt_2024_associated_publication.csv")
+
+# link to bibtex guide
+# https://www.bibtex.com/e/entry-types/
