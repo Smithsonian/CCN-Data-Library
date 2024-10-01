592819311b4823a51cb8631d189b05a254f86417
cheneyr <cheneyr@si.edu>
2024-10-01 17:02:20 -0400

    update Fu et al and version info

--- scripts/0_data_hooks/Fu_et_al_2023_hook.R
+++ scripts/0_data_hooks/Fu_et_al_2023_hook.R
@@ -12,6 +12,7 @@ library(tidyverse)
 library(readxl)
 library(lubridate)
 library(leaflet)
+library(janitor)
 
 # load in helper functions
 source("scripts/1_data_formatting/curation_functions.R") # For curation
@@ -30,6 +31,7 @@ id <- "Fu_et_al_2023"
 # curate materials and methods
 methods <- tibble(
   study_id = id,
+  method_id = "single set of methods",
   coring_method = "push core",
   roots_flag = "roots and rhizomes separated", #visible inorganic material and shells also removed 
   compaction_flag = "compaction qualified",
@@ -47,23 +49,54 @@ methods <- reorderColumns("methods", methods)
 
 ## ... Depthseries ####
 
-
+#remove filler/formatting rows -- need units for pb and ra 
+#curate pb210 data
 pb_depth <- data_pb %>% 
-  filter(!`Bahamas Site 1` == "Internal diameter")
+  janitor::remove_empty(which = c("cols", "rows")) %>% 
+  slice(-c(1, 3, 57:60, 112:115, 158:161, 187:190, 241:244, 293:296, 343:346, 373:376)) %>% 
+  row_to_names(row_number = 1) %>% clean_names() %>% 
+  rename(total_pb210_activity = total_210pb_bulk,
+         total_pb210_activity_se = na_2,
+         ra226_activity = ra_226,
+         ra226_activity_se = na_4,
+         excess_pb210_activity = excess_210pb_bulk,
+         excess_pb210_activity_se = na_6) %>% 
+  mutate(study_id = id,
+         method_id = "single set of methods",
+         depth_min = as.numeric(depth_top),
+         depth_max = as.numeric(depth_bottom),
+         site_id = case_when(grepl("Site 1 ", code) ~ "S1",
+                             grepl("Site 2", code) ~ "S2",
+                             grepl("Site 3", code) ~ "S3",
+                             grepl("Site 4", code) ~ "S4",
+                             grepl("Site 5", code) ~ "S5",
+                             grepl("Site 6", code) ~ "S6",
+                             grepl("Site 7", code) ~ "S7",
+                             grepl("Site 8", code) ~ "S8", 
+                             grepl("Site 9", code) ~ "S9",
+                             TRUE ~ "S10"),
+         core_id = paste0(site_id, "_Pb")) %>% 
+  select(-depth, -depth_top, -depth_bottom, - code, -na, -na_3, -na_5, -dry_mass, -accumulated_mass, -accumulated_mass_2)
 
-
-
+#reorder and check table names
+pb_depth <- reorderColumns("depthseries", pb_depth)
+  
+  
+#curate other depthseries data 
 depthseries_curate <- data1 %>% 
   filter(!Depth == "cm") %>%  #filter out unit row
   rename(site_id = Site,
          core_id = Core,
-         organic_carbon = Corg,
+         fraction_carbon = Corg,
          dry_bulk_density = `Dry bulk density`,
          delta_c13 = `13C`) %>% 
   fill(core_id, site_id) %>% 
   mutate(study_id = id,
          method_id = "single set of methods",
          core_id = paste(site_id, core_id, sep = "_"),
+         fraction_carbon = as.numeric(fraction_carbon)/100, #get percent C
+         pb210_unit = "bequerelsPerKilogram",
+         ra226_unit = "bequerelsPerKilogram",
          depth_min = case_when(Depth == "0-1" ~ 0, #fix encoding, error occurred when reading xlsx into r 
                                Depth == "44563"|Depth == "1-3.5" ~ 1,
                                Depth == "44595" ~ 2,
@@ -101,10 +134,12 @@ depthseries_curate <- data1 %>%
   select(-N, -P, -`N/P molar`, -Ca, -Mg, -`Ca/Mg molar`, -Cinorg) %>% #remove uncontrolled variables 
   select(-Depth)
 
+#should have 10 pb cores, one from each site, currently missing 4, 9, 10 
+
+depthseries <- full_join(depthseries_curate, pb_depth)
 
 
-
-depthseries <- reorderColumns("depthseries", depthseries_curate)
+depthseries <- reorderColumns("depthseries", depthseries)
 
 
 ## ... Cores ####
@@ -118,8 +153,29 @@ cores <- depthseries %>%
          year = 2021,
          month = 11,
          vegetation_class = "seagrass",
-         vegetation_method = "field observation")
-
+         vegetation_method = "field observation",
+         position_method = "other low resolution",
+         position_notes = "position at site level",
+         longitude = case_when(site_id == "S1" ~ -75.70761,
+                              site_id == "S2" ~ -75.80477,
+                              site_id == "S3" ~ -75.86002,
+                              site_id == "S4" ~ -75.75391,
+                              site_id == "S5" ~ -76.07485,
+                              site_id == "S6" ~ -76.04701,
+                              site_id == "S7" ~ -76.10755,
+                              site_id == "S8" ~ -76.12758,
+                              site_id == "S9" ~ -76.02600,
+                              site_id == "S10" ~ -76.03386),
+         latitude = case_when(site_id == "S1" ~ 23.49614,
+                               site_id == "S2" ~ 23.53387,
+                               site_id == "S3" ~ 23.58456,
+                               site_id == "S4" ~ 23.51049,
+                               site_id == "S5" ~ 23.74173,
+                               site_id == "S6" ~ 23.73557,
+                               site_id == "S7" ~ 23.76910,
+                               site_id == "S8" ~ 23.78310,
+                               site_id == "S9" ~ 23.72511,
+                               site_id == "S10" ~ 23.72584))
 
 cores <- reorderColumns("cores", cores)
 
@@ -158,7 +214,8 @@ testRequired(table_names)
 testConditional(table_names)
 
 testUniqueCoords(cores)
-testIDs(cores, depthseries, by = "core_id")
+testIDs(cores, depthseries, by = "core")
+testIDs(cores, depthseries, by = "site")
 fraction_not_percent(depthseries)
 results <- test_numeric_vars(depthseries)
 
